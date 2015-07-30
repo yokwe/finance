1b1904b19fe063e0cc7754cedf444c67aca61a94
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-07-30 16:01:44 +0900

    Add empty HV.java

--- HistoricalData/src/yokwe/finance/historicalData/A001.java
+++
@@ -1,9 +0,0 @@
-package yokwe.finance.historicalData;
-
-public class A001 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-
-}
---
+++ HistoricalData/src/yokwe/finance/historicalData/HV.java
@@ -0,0 +1,12 @@
+package yokwe.finance.historicalData;
+
+import org.slf4j.LoggerFactory;
+
+public class HV {
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(HV.class);
+	
+	public static void main(String[] args) {
+		logger.info("START");
+		logger.info("STOP");
+	}
+}

688ad9c6582e04e3b70c8f7601f4e5523b78e8f0
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-08-26 16:45:33 +0900

    Temporary commit.

--- HistoricalData/data/sql/create-table.sql
+++ HistoricalData/data/sql/create-table.sql
@@ -5,12 +5,11 @@ CREATE TABLE etf_info (
   symbol           TEXT     NOT NULL, -- ticker symbol
   listed_exchange  TEXT     NOT NULL, -- name of exchange NYSEMKT NYSE NYSEARCA NASDAQ BATS
   name             TEXT     NOT NULL, -- name
-  category         TEXT     NOT NULL, -- category
+  category         TEXT     NOT NULL, -- category (morning star)
   summary          TEXT     NOT NULL, -- summary
   inception_date   TEXT     NOT NULL, -- inception date as YYYY-MM-DD
   expense_ratio    INTEGER  NOT NULL  -- in 100th of percent  12.34% = 123
-  dividend-frequency 
-);
+ );
 
 CREATE TABLE etf_data_daily (
   symbol    TEXT     NOT NULL, -- ticker symbol
@@ -21,5 +20,6 @@ CREATE TABLE etf_data_daily (
   close     INTEGER  NOT NULL, -- in 100th of value  123.45 = 12345
   volume    INTEGER  NOT NULL
 );
+
 
 BEGIN{X["A"]="NYSEMKT";X["N"]="NYSE";X["P"]="NYSEARCA";X["Q"]="NASDAQ";X["Z"]="BATS"

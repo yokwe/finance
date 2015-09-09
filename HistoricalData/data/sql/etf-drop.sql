9309f0e9f048e31164819903493329677ccdbfee
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-09-09 16:45:21 +0900

    Fix table name.

--- HistoricalData/data/sql/etf-drop.sql
+++ HistoricalData/data/sql/etf-drop.sql
@@ -7,10 +7,10 @@
 
 DROP INDEX IF EXISTS etf_etf_symbol;
 DROP INDEX IF EXISTS etf_yahoo_profile_symbol;
-DROP INDEX IF EXISTS etf_yahoo_dialy_symbol_date;
+DROP INDEX IF EXISTS etf_yahoo_daily_symbol_date;
 DROP INDEX IF EXISTS etf_yahoo_dividend_symbol_date;
 
 DROP TABLE IF EXISTS etf_etf;
 DROP TABLE IF EXISTS etf_yahoo_profile;
-DROP TABLE IF EXISTS etf_yahoo_dialy;
+DROP TABLE IF EXISTS etf_yahoo_daily;
 DROP TABLE IF EXISTS etf_yahoo_dividend;

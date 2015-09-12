1699010515f35c15405aecf70fac00cdf896301c
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-09-12 19:15:58 +0900

    Download only non-existing file in Downloader.

--- HistoricalData/src/yokwe/finance/etf/Downloader.java
+++ HistoricalData/src/yokwe/finance/etf/Downloader.java
@@ -3,9 +3,12 @@ package yokwe.finance.etf;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,19 +19,22 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.slf4j.LoggerFactory;
 
+
 public class Downloader {
 	static final org.slf4j.Logger logger = LoggerFactory.getLogger(Downloader.class);
 
 	protected static final CloseableHttpClient httpclient = HttpClients.createDefault();
 	
-	public static void download(String url, String fileName) {
+	public static void download(String url, String fileName, List<String> messageList) {
 		HttpGet httpget = new HttpGet(url);
 		try (CloseableHttpResponse response = httpclient.execute(httpget)) {
 			final int code = response.getStatusLine().getStatusCode();
+			final String reasonPhrase = response.getStatusLine().getReasonPhrase();
 			
 			if (code == 404) {
-				logger.debug("statusLine = {}", response.getStatusLine().toString());
-				logger.error("url {}", url);
+				String message = String.format("%d %s - %s", code, reasonPhrase, fileName);
+				logger.error(message);
+				messageList.add(message);
 				return;
 			}
 			if (code != 200) {
@@ -68,10 +74,18 @@ public class Downloader {
 	private static final Matcher matcherParam = Pattern.compile("^([^ ]+) +([^ ]+)$").matcher("");
 	private static final Matcher matcherComment = Pattern.compile("^#.+$").matcher("");
 	
+	static class Entry {
+		final String url;
+		final String file;
+		Entry(String url, String file) {
+			this.url = url;
+			this.file = file;
+		}
+	}
 	public static void download(String path) {
+		// Build entryList
+		List<Entry> entryList = new ArrayList<>();
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			int count = 0;
-			long lastDownload = 0;
 			for(;;) {
 				String line = br.readLine();
 				if (line == null) break;
@@ -84,24 +98,50 @@ public class Downloader {
 					throw new ETFException("");
 				}
 				
+				String file = matcherParam.group(1);
+				String url = matcherParam.group(2);
+				
+				entryList.add(new Entry(url, file));
+			}
+		} catch (IOException e) {
+			logger.info("IOException {}", e.toString());
+		}
+		
+		// Process entryList
+		List<String> messageList = new ArrayList<>();
+		try {
+			int totalCount = entryList.size();
+			int count = 0;
+			long lastDownload = 0;
+			for(Entry entry: entryList) {				
 				count++;
+				{
+					File file = new File(entry.file);
+					if (file.exists()) continue;
+				}
+				
 				long now = System.currentTimeMillis();
 				long waitTime = 1000 - (now - lastDownload);
 				if (0 < waitTime) Thread.sleep(waitTime);
 				
-				String file = matcherParam.group(1);
-				String url = matcherParam.group(2);
+				String url = entry.url;
+				String file = entry.file;
 				
 				lastDownload = System.currentTimeMillis();
-				logger.info("{}", String.format("%5d %s", count, file));
-				download(url, file);
+				logger.info("{}", String.format("%5d / %5d  %s", count, totalCount, file));
+				download(url, file, messageList);
 			}
-		} catch (UnsupportedOperationException e) {
-			logger.info("UnsupportedOperationException {}", e.toString());
-		} catch (IOException e) {
-			logger.info("IOException {}", e.toString());
 		} catch (InterruptedException e) {
 			logger.info("InterruptedException {}", e.toString());
+		}
+		
+		// Print messageList
+		{
+			int totalCount = messageList.size();
+			int count = 0;
+			for(String message: messageList) {
+				logger.error("{}", String.format("%5d / %5d  %s", count, totalCount, message));
+			}
 		}
 	}
 	

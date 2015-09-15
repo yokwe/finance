ae3162f5540d6015d7b96d4f1d4b9afc85dedaa4
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-09-15 12:04:01 +0900

    Add method for standard deviation.

--- ETF/src/yokwe/finance/etf/Util.java
+++ ETF/src/yokwe/finance/etf/Util.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.LoggerFactory;
 
@@ -28,5 +30,30 @@ public final class Util {
 			throw new ETFException();
 		}
 		return ret.toString();
+	}
+	
+	public static double StandardDeviationSample(List<Double> valueList) {
+		return StandardDeviation(valueList, 2);
+	}
+	public static double StandardDeviationPopulation(List<Double> valueList) {
+		return StandardDeviation(valueList, 1);
+	}
+	public static double StandardDeviation(List<Double> valueList, final int offset) {
+	    double M = 0.0;
+	    double S = 0.0;
+	    int k = 1;
+	    for (double value: valueList) {
+	        double tmpM = M;
+	        M += (value - tmpM) / k;
+	        S += (value - tmpM) * (value - M);
+	        k++;
+	    }
+	    return Math.sqrt(S / (k-offset));
+	}
+	
+	public static void main(String[] args) {
+		Double[] data = new Double[] {53.0, 61.0, 49.0, 67.0, 55.0, 63.0};
+		logger.info("SD = {}", StandardDeviationSample(Arrays.asList(data)));
+		logger.info("SD = {}", StandardDeviationPopulation(Arrays.asList(data)));
 	}
 }

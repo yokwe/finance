e3864b3e91e7f5b1002090b27355e3f197cf0385
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-09-16 18:48:31 +0900

    Temporary commit(still not working as expected)

--- ETF/src/yokwe/finance/etf/util/StreamUtil.java
+++ ETF/src/yokwe/finance/etf/util/StreamUtil.java
@@ -44,8 +44,9 @@ public class StreamUtil {
 			double term1 = delta * delta_n * (n - 1);
 			
 			M1 += delta_n;
-			M4 += term1 * delta_n2 * (n * n - 3 * n + 3) + (6 * delta_n2 * M2) - (4 * delta_n * M3);
-			M3 += term1 * delta_n * (n - 2) - 3 * delta_n * M2;
+//			M4 += - (4 * M3 * delta_n) + (6 * M2 * delta_n2) + (term1 * delta_n2 * ((n * n) - (3 * n) + 3));
+			M4 += - (4 * M3 * delta_n) + (6 * M2 * delta_n2) + ((n - 1) * ((n * n) - (3 * n) + 3) * delta_n * delta_n2 * delta);
+			M3 += - (3 * M2 * delta_n) + (term1 * delta_n * (n - 2));
 			M2 += term1;
 		}
 		

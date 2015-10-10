1948d779b4b5c1f827356f25e1ab8bd3bc6e2f7f
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-10-10 17:28:14 +0900

    Add year stats data.

--- ETF/src/yokwe/finance/etf/app/EstimateDividend.java
+++ ETF/src/yokwe/finance/etf/app/EstimateDividend.java
@@ -19,11 +19,7 @@ import yokwe.finance.etf.web.DividendData;
 
 public class EstimateDividend {
 	private static final Logger logger = LoggerFactory.getLogger(EstimateDividend.class);
-	
-	private static final String SQL_SYMBOL_LIST = "select * from yahoo_dividend where date like '%d%%' group by symbol having count(*) = %d order by symbol, date";
-	private static final String SQL_BY_YEAR     = "select * from yahoo_dividend where date like '%d%%' and symbol = '%s' order by date";
-	private static final String SQL_DAILY       = "select date, symbol, close as value from yahoo_daily where symbol = '%s' and '%s' < date and date <= '%s'";
-	
+		
 	public static class LastDailyDay {
 		private static String SQL_STATEMENT = "select max(date) as last_day from yahoo_daily";
 		
@@ -63,48 +59,68 @@ public class EstimateDividend {
 		return String.format("%4d-%02d-%02d", calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1,
 				calendar.get(Calendar.DAY_OF_MONTH));
 	}
+	
+	static Calendar getLastTradedDay(final Statement statement) {
+		String sql = LastDailyDay.genSQL();
+		String lastTradedDate = JDBCUtil.getResultAll(statement, sql, LastDailyDay.class).get(0).last_day;
+		
+		int year  = Integer.parseInt(lastTradedDate.substring(0, 4));
+		int month = Integer.parseInt(lastTradedDate.substring(5, 7));
+		int day   = Integer.parseInt(lastTradedDate.substring(8, 10));
+		
+		Calendar ret = Calendar.getInstance();
+		ret.set(Calendar.YEAR,  year);
+		ret.set(Calendar.MONTH, month - 1); // 0 base
+		ret.set(Calendar.DATE,  day);
+		
+		return ret;
+	}
+	
+	static Calendar add(final Calendar origin, final int field, final int amount) {
+		Calendar ret = Calendar.getInstance();
+		ret.setTimeInMillis(origin.getTimeInMillis());
+		ret.add(field, amount);
+		return ret;
+	}
+	
+	static List<String> getSymbol(final Statement statement, final int year, final int freq) {
+		final String sql_template = "select * from yahoo_dividend where date like '%d%%' group by symbol having count(*) = %d order by symbol, date";
+		final String sql = String.format(sql_template, year, freq);
+		List<String> ret = JDBCUtil.getResultAll(statement, sql, DividendData.class).stream().map(o -> o.symbol).collect(Collectors.toList());
+		return ret;
+	}
+	
+	static List<DividendData> getDividendData(final Statement statement, final String symbol, final int year) {
+		final String sql_template = "select * from yahoo_dividend where date like '%d%%' and symbol = '%s' order by date";
+		final String sql = String.format(sql_template, year, symbol);
+		List<DividendData> ret = JDBCUtil.getResultAll(statement, sql, DividendData.class).stream().collect(Collectors.toList());
+		return ret;
+	}
+	
+	static double[] getDailyData(final Statement statement, final String symbol, final Calendar dateBegin, final Calendar dateEnd) {
+		String sql_template = "select date, symbol, close as value from yahoo_daily where symbol = '%s' and '%s' < date and date <= '%s'";
+		String sql = String.format(sql_template, symbol, dateString(dateBegin), dateString(dateEnd));
+		double[] ret = JDBCUtil.getResultAll(statement, sql, DailyData.class).stream().mapToDouble(o -> o.value).toArray();
+		return ret;
+	}
+	
 
 	static void estimate(final Statement statement, final int freq) {
-		final String lastDailyDate;
-		final String lastDailyDateOneWeekBefore;
-		final Calendar now = Calendar.getInstance();
-		final int thisYearNo = now.get(Calendar.YEAR);
-		final int lastYearNo = thisYearNo - 1;
+		final Calendar origin = getLastTradedDay(statement);
 		
-		{
-			String sql = LastDailyDay.genSQL();
-			lastDailyDate = JDBCUtil.getResultAll(statement, sql, LastDailyDay.class).get(0).last_day;
-			logger.info("lastDailyDate = {}", lastDailyDate);
-			
-			int year  = Integer.parseInt(lastDailyDate.substring(0, 4));
-			int month = Integer.parseInt(lastDailyDate.substring(5, 7));
-			int day   = Integer.parseInt(lastDailyDate.substring(8, 10));
-			
-			Calendar oneWeekBefore = Calendar.getInstance();
-			oneWeekBefore.set(Calendar.YEAR,  year);
-			oneWeekBefore.set(Calendar.MONTH, month - 1); // 0 base
-			oneWeekBefore.set(Calendar.DATE,  day);
-			oneWeekBefore.add(Calendar.DATE, -7);
-			
-			lastDailyDateOneWeekBefore = dateString(oneWeekBefore);
-			
-			logger.info("lastDailyDateOneWeekBefore = {}", lastDailyDateOneWeekBefore);
-		}
-
-		final List<String> symbolList;
-		{
-			String sql = String.format(SQL_SYMBOL_LIST, lastYearNo, freq);
-			symbolList = JDBCUtil.getResultAll(statement, sql, DividendData.class).stream().map(o -> o.symbol).collect(Collectors.toList());
-		}
+		final int thisYearNo   = origin.get(Calendar.YEAR);
+		final int lastYearNo   = thisYearNo - 1;
+		final Calendar oneWeek = add(origin, Calendar.DATE, -7);
+		final Calendar oneYear = add(origin, Calendar.YEAR, -1);
+		
+		final List<String> symbolList = getSymbol(statement, lastYearNo, freq);
 		logger.info("symbolList = {}", symbolList.size());
 		
 		for(String symbol: symbolList) {
 			// data of last year
-			String sqlLastYear = String.format(SQL_BY_YEAR, lastYearNo, symbol);
-			List<DividendData> lastYear = JDBCUtil.getResultAll(statement, sqlLastYear, DividendData.class).stream().collect(Collectors.toList());
+			List<DividendData> lastYear = getDividendData(statement, symbol, lastYearNo);
 			// data of this year
-			String sqlThisYear = String.format(SQL_BY_YEAR, thisYearNo, symbol);
-			List<DividendData> thisYear = JDBCUtil.getResultAll(statement, sqlThisYear, DividendData.class).stream().collect(Collectors.toList());
+			List<DividendData> thisYear = getDividendData(statement, symbol, thisYearNo);
 			
 			// sanity check
 			if (lastYear.size() != freq) {
@@ -116,25 +132,30 @@ public class EstimateDividend {
 			
 			Stats statsLastYear = new Stats(lastYear.stream().mapToDouble(o -> o.dividend).toArray());
 
+			// build annual data of this year from data of last year
 			lastYear.subList(0, thisYear.size()).clear();
 			thisYear.addAll(lastYear);
 			thisYear.sort((o1, o2) -> o1.date.compareTo(o2.date));
 			
 			Stats statsThisYear = new Stats(thisYear.stream().mapToDouble(o -> o.dividend).toArray());
 			
-			String sqlDaily = String.format(SQL_DAILY, symbol, lastDailyDateOneWeekBefore, lastDailyDate);
-			double[] dailyData = JDBCUtil.getResultAll(statement, sqlDaily, DailyData.class).stream().mapToDouble(o -> o.value).toArray();
-			if (dailyData.length < 5) {
-				logger.info("sql = {}", sqlDaily);
+			double[] weekData  = getDailyData(statement, symbol, oneWeek, origin);
+			Stats    statsWeek = new Stats(weekData);
+			if (weekData.length < 5) {
+				logger.info("symbol = {}", symbol);
 				throw new ETFException("dailyData");
 			}
 			
-			Stats statsDaily = new Stats(dailyData);
+			double[] yearData  = getDailyData(statement, symbol, oneYear, origin);
+			Stats    statsYear = new Stats(yearData);
 			
-			final double price = statsDaily.avg;
+			final double price = statsWeek.avg;
 			final double divLastYear = statsLastYear.sum / price * 100;
 			final double divThisYear = statsThisYear.sum / price * 100;
-			logger.info("{}", String.format("%-6s  %6.2f  %6.2f  %6.2f  -  %6.2f  %6.2f  %6.2f", symbol, price, divLastYear, divThisYear, statsThisYear.avg, statsThisYear.sd, (statsThisYear.sd / statsThisYear.avg)));			
+			logger.info("{}", String.format("%-6s  %6.2f  %6.2f  %6.2f  -  %6.2f  %6.2f  %6.2f  -  %6.2f  %6.2f  %6.3f",
+					symbol, price, divLastYear, divThisYear,
+					statsThisYear.avg, statsThisYear.sd, (statsThisYear.sd / statsThisYear.avg),
+					statsYear.avg,     statsYear.sd,     (statsYear.sd     / statsYear.avg)));			
 		}
 	}
 

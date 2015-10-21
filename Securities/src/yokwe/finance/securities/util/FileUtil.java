aec1bb44f328cd5ed238b893cb09b4097ce50aab
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-10-21 16:55:56 +0900

    Add FileUtil.getNasdaqInfo()

--- Securities/src/yokwe/finance/securities/util/FileUtil.java
+++ Securities/src/yokwe/finance/securities/util/FileUtil.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,5 +34,70 @@ public class FileUtil {
 		}
 		return ret.toString();
 	}
-
+	
+	public static class NasdaqInfo {
+		public final String etf;
+		public final String exch;
+		public final String symbol;
+		public final String yahoo;
+		public final String google;
+		public final String nasdaq;
+		public final String name;
+		
+		NasdaqInfo(String etf, String exch, String symbol, String yahoo, String google, String nasdaq, String name) {
+			this.etf    = etf;
+			this.exch   = exch;
+			this.symbol = symbol;
+			this.yahoo  = yahoo;
+			this.google = google;
+			this.nasdaq = nasdaq;
+			this.name   = name;
+		}
+		
+		public String toString() {
+			return String.format("%s,%s,%s,%s,%s,%s,%s", etf, exch, symbol, yahoo, google, nasdaq, name);
+		}
+	}
+	
+	private static final String NASDAQ_FILE_PATH = "tmp/sqlite/nasdaq.csv";
+	public static Map<String, NasdaqInfo> getNasdaqInfo() {
+		Map<String, NasdaqInfo> ret = new TreeMap<>();
+		
+		char[] buffer = new char[65536];
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(new File(NASDAQ_FILE_PATH)), buffer.length)) {
+			for(;;) {
+				String line = br.readLine();
+				if (line == null) break;
+				
+				String[] fields = line.split(",", 7);
+				String etf    = fields[0];
+				String exch   = fields[1];
+				String symbol = fields[2];
+				String yahoo  = fields[3];
+				String google = fields[4];
+				String nasdaq = fields[5];
+				String name   = fields[6];
+				
+				if (name.charAt(0) == '"') name = name.substring(1, name.length() - 1);
+				if (name.contains("\"\"")) name = name.replace("\"\"", "\"");
+				
+				ret.put(symbol, new NasdaqInfo(etf, exch, symbol, yahoo, google, nasdaq, name));
+			}
+		} catch (IOException e) {
+			logger.error(e.getClass().getName());
+			logger.error(e.getMessage());
+			throw new SecuritiesException();
+		}
+		
+		return ret;
+	}
+	
+//	public static void main(String[] args) {
+//		Map<String, NasdaqInfo> map = getNasdaqInfo();
+//		for(String symbol: map.keySet()) {
+//			NasdaqInfo info = map.get(symbol);
+//			logger.info("{}  {}", symbol, info);
+//		}
+//	}
 }

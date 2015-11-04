3976a829b7773d08f3fc067468a301f08babc6cd
yokwe <yokew@github.com>
2015-11-04 15:58:18 +0900

    Fix year of date field.

--- Securities/src/yokwe/finance/securities/update/GoogleHistorical.java
+++ Securities/src/yokwe/finance/securities/update/GoogleHistorical.java
@@ -6,12 +6,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.slf4j.LoggerFactory;
 
 import yokwe.finance.securities.SecuritiesException;
+import yokwe.finance.securities.database.PriceTable;
 
 public final class GoogleHistorical {
 	static final org.slf4j.Logger logger = LoggerFactory.getLogger(GoogleHistorical.class);
@@ -24,6 +26,7 @@ public final class GoogleHistorical {
 //	13-Oct-15,-,-,-,37.83,0
 //	12-Oct-15,38.06,38.06,37.59,37.83,5581
 
+	private static final int THIS_YEAR = LocalDate.now().getYear();
 	
 	public static final class CSVRecord {
 		public static final String HEADER = "\uFEFFDate,Open,High,Low,Close,Volume";
@@ -39,7 +42,7 @@ public final class GoogleHistorical {
 		private static final DateTimeFormatter parseDate  = DateTimeFormatter.ofPattern("d-MMM-yy");
 		private static final DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-		public static String toCSV(String symbol, String line) {
+		public static PriceTable toPriceTable(String symbol, String line) {
 			String[] fields = line.split(",");
 			if (fields.length != NUMBER_OF_FIELDS) {
 				logger.error("fields  {}  line = {}", symbol, line);
@@ -47,7 +50,12 @@ public final class GoogleHistorical {
 			}
 			
 			// Fix format of date  02-Jan-14 => 2014-01-02
-			fields[0] = formatDate.format(parseDate.parse(fields[0]));
+			// Fix year of date    02-Jan-80 => 1980-01-02
+			{
+				LocalDate localDate = LocalDate.from(parseDate.parse(fields[0]));
+				if (THIS_YEAR < localDate.getYear()) localDate = localDate.minusYears(100);
+				fields[0] = formatDate.format(localDate);
+			}
 			
 			// Special when field (open, high and low) contains dash
 			if (fields[1].equals("-")) {
@@ -69,11 +77,13 @@ public final class GoogleHistorical {
 //			double open     = Double.valueOf(fields[1]);
 //			double high     = Double.valueOf(fields[2]);
 //			double low      = Double.valueOf(fields[3]);
-			double close    = Double.valueOf(fields[4]);
-			long   volume   = Long.valueOf(fields[5]);
+			final double close    = Double.valueOf(fields[4]);
+			final long   volume   = Long.valueOf(fields[5]);
 			
-//			return String.format("%s,%s,%.2f,%.2f,%.2f,%.2f,%d", date, symbol, open, high, low, close, volume);
-			return String.format("%s,%s,%.2f,%d", date, symbol, close, volume);
+			return new PriceTable(date, symbol, close, volume);
+		}
+		public static String toCSV(String symbol, String line) {
+			return toPriceTable(symbol, line).toCSV();
 		}
 	}
 	

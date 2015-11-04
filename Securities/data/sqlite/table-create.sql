d759a9f9178200fa4f35442ffdb6d75d9a02cf92
yokwe <yokew@github.com>
2015-11-04 16:28:41 +0900

    Load price data from tmp/database/price-XXXX.csv

--- Securities/data/sqlite/table-create.sql
+++ Securities/data/sqlite/table-create.sql
@@ -45,8 +45,58 @@ CREATE TABLE dividend (
 .import tmp/database/nasdaq.csv           nasdaq
 .import tmp/database/google-finance.csv   finance
 .import tmp/database/yahoo-dividend.csv   dividend
-.import tmp/database/yahoo-daily.csv      price
+-- .import tmp/database/yahoo-daily.csv      price
 -- .import tmp/database/google-getprices.csv price
+
+.import tmp/database/price-1975.csv       price
+.import tmp/database/price-1976.csv       price
+.import tmp/database/price-1977.csv       price
+.import tmp/database/price-1978.csv       price
+.import tmp/database/price-1979.csv       price
+
+.import tmp/database/price-1980.csv       price
+.import tmp/database/price-1981.csv       price
+.import tmp/database/price-1982.csv       price
+.import tmp/database/price-1983.csv       price
+.import tmp/database/price-1984.csv       price
+.import tmp/database/price-1985.csv       price
+.import tmp/database/price-1986.csv       price
+.import tmp/database/price-1987.csv       price
+.import tmp/database/price-1988.csv       price
+.import tmp/database/price-1989.csv       price
+
+.import tmp/database/price-1990.csv       price
+.import tmp/database/price-1991.csv       price
+.import tmp/database/price-1992.csv       price
+.import tmp/database/price-1993.csv       price
+.import tmp/database/price-1994.csv       price
+.import tmp/database/price-1995.csv       price
+.import tmp/database/price-1996.csv       price
+.import tmp/database/price-1997.csv       price
+.import tmp/database/price-1998.csv       price
+.import tmp/database/price-1999.csv       price
+
+.import tmp/database/price-2000.csv       price
+.import tmp/database/price-2001.csv       price
+.import tmp/database/price-2002.csv       price
+.import tmp/database/price-2003.csv       price
+.import tmp/database/price-2004.csv       price
+.import tmp/database/price-2005.csv       price
+.import tmp/database/price-2006.csv       price
+.import tmp/database/price-2007.csv       price
+.import tmp/database/price-2008.csv       price
+.import tmp/database/price-2009.csv       price
+
+.import tmp/database/price-2010.csv       price
+.import tmp/database/price-2011.csv       price
+.import tmp/database/price-2012.csv       price
+.import tmp/database/price-2013.csv       price
+.import tmp/database/price-2014.csv       price
+.import tmp/database/price-2015.csv       price
+.import tmp/database/price-2016.csv       price
+.import tmp/database/price-2017.csv       price
+.import tmp/database/price-2018.csv       price
+.import tmp/database/price-2019.csv       price
 
 CREATE        INDEX nasdaq_etf           ON nasdaq(etf);
 CREATE UNIQUE INDEX nasdaq_symbol        ON nasdaq(symbol);

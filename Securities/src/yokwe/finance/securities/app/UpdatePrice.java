c80d1075d4c99550f7147027fbd7d634b53ea27e
yokwe <yokew@github.com>
2015-11-09 11:59:28 +0900

    Handle empty file case.

--- Securities/src/yokwe/finance/securities/app/UpdatePrice.java
+++ Securities/src/yokwe/finance/securities/app/UpdatePrice.java
@@ -24,65 +24,19 @@ import yokwe.finance.securities.database.NasdaqTable;
 import yokwe.finance.securities.database.PriceTable;
 import yokwe.finance.securities.update.GoogleHistorical;
 import yokwe.finance.securities.update.YahooDaily;
-import yokwe.finance.securities.util.HttpUtil;
 import yokwe.finance.securities.util.NasdaqUtil;
 
-public class UpdatePrice {
+public final class UpdatePrice {
 	private static final Logger logger = LoggerFactory.getLogger(UpdatePrice.class);
 	
-	// Use yahoo-table
-	static Map<String, PriceTable> getFromYahoo(final String exch, final String symbol, final String yahooSymbol, final LocalDate dateFrom, final LocalDate dateTo) {
-		// http://real-chart.finance.yahoo.com/table.csv?s=SPY&a=00&b=01&c=2015&d=12&e=30&f=2015&ignore=.csv
-		final String s = yahooSymbol;
-		final int a = dateFrom.getMonthValue() - 1;
-		final int b = dateFrom.getDayOfMonth();
-		final int c = dateFrom.getYear();
-		final int d = dateTo.getMonthValue() - 1;
-		final int e = dateTo.getDayOfMonth();
-		final int f = dateTo.getYear();
-		
-		final String url = String.format("http://real-chart.finance.yahoo.com/table.csv?s=%s&a=%02d&b=%02d&c=%d&d=%02d&e=%02d&f=%d&g=d&ignore=.csv", s, a, b, c, d, e, f);
-		final List<String> lines = HttpUtil.download(url);
-		
-		final Map<String, PriceTable> ret = new HashMap<>();
-		if (lines == null) return ret;
-		
-		int count = 0;
-		for(String line: lines) {
-			if (count++ == 0) {
-				YahooDaily.CSVRecord.checkHeader(line);
-			} else {
-				final PriceTable priceTable = YahooDaily.CSVRecord.toPriceTable(symbol, line);
-				ret.put(priceTable.date, priceTable);
-			}
-		}
-		return ret;
+	static enum Provider {
+		YAHOO, GOOGLE,
 	}
-	
-	static Map<String, PriceTable> getFromGoogle(final String exch, final String symbol, final String googleSymbol, final LocalDate dateFrom, final LocalDate dateTo) {
-		// http://www.google.com/finance/historical?q=NYSE:IBM&startdate=Jan%201,%202000&enddate=Dec%2031,%202050&output=csv
-		final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MMM d, yyyy", Locale.US);
-
-		final String startdate = dateFrom.format(dateFormatter).replace(" ", "%20");
-		final String enddate   = dateTo.format(dateFormatter).replace(" ", "%20");
-		final String url = String.format("http://www.google.com/finance/historical?q=%s:%s&startdate=%s&enddate=%s&output=csv", exch, googleSymbol, startdate, enddate);
-		List<String> lines = HttpUtil.download(url);
-
-		final Map<String, PriceTable> ret = new HashMap<>();
-		if (lines == null) return ret;
-		
-		int count = 0;
-		for(String line: lines) {
-			if (count++ == 0) {
-				GoogleHistorical.CSVRecord.checkHeader(line);
-			} else {
-				final PriceTable priceTable = GoogleHistorical.CSVRecord.toPriceTable(symbol, line);
-				ret.put(priceTable.date, priceTable);
-			}
-		}
-		return ret;
+	// price-2015-VCLT-yahoo.html
+	private static String HTML_FILE_PATH_FORMAT = "tmp/update/price-%d-%s-%s.csv";
+	static String getFilePath(final int year, final String symbol, final Provider provider) {
+		return String.format(HTML_FILE_PATH_FORMAT, year, symbol, provider.name());
 	}
-	
 	
 	static List<String> getDateList(Map<String, Map<String, PriceTable>> map) {
 		Map<String, PriceTable> ibmMap = map.get("IBM");
@@ -132,10 +86,7 @@ public class UpdatePrice {
 		final LocalDate dateFrom = LocalDate.of(year, 1, 1);
 		final LocalDate dateTo   = dateFrom.plusYears(1).minusDays(1);
 		
-		// Download price file from yahoo and google
-		//   tmp/update/google-yyyy-symbol.html
-		//   tmp/update/yahoo-yyyy-symbol.html
-		
+		// Download price file from yahoo and google		
 		logger.info("Build pathURLMap");
 		Map<String, PathURL> yahooPathURLMap  = new TreeMap<>();
 		Map<String, PathURL> googlePathURLMap = new TreeMap<>();
@@ -143,7 +94,7 @@ public class UpdatePrice {
 			NasdaqTable nasdaq = nasdaqMap.get(symbol);
 			// yahoo
 			{
-				final String path = String.format("tmp/update/yahoo-%d-%s.html", year, symbol);
+				final String path = getFilePath(year, symbol, Provider.YAHOO);
 				
 				// http://real-chart.finance.yahoo.com/table.csv?s=SPY&a=00&b=01&c=2015&d=12&e=30&f=2015&ignore=.csv
 				final String s = nasdaq.yahoo;
@@ -160,7 +111,7 @@ public class UpdatePrice {
 			}
 			// google
 			{
-				final String path = String.format("tmp/update/google-%d-%s.html", year, symbol);
+				final String path = getFilePath(year, symbol, Provider.GOOGLE);
 				
 				// http://www.google.com/finance/historical?q=NYSE:IBM&startdate=Jan%201,%202000&enddate=Dec%2031,%202050&output=csv
 				final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MMM d, yyyy", Locale.US);
@@ -175,19 +126,26 @@ public class UpdatePrice {
 		
 		// Fetch
 		logger.info("Fetch");
-		for(String symbol: nasdaqMap.keySet()) {
-			{
-				PathURL pathURL = yahooPathURLMap.get(symbol);
-				File file = new File(pathURL.path);
-				if (!file.exists()) {
-					Fetch.download(pathURL.url, pathURL.path, new ArrayList<>());
+		{
+			final int total = nasdaqMap.keySet().size();
+			int count = 0;
+			for(String symbol: nasdaqMap.keySet()) {
+				if ((count++ % 100) == 0) {
+					logger.info("{}", String.format("%4d / %4d  %s", count, total, symbol));
 				}
-			}
-			{
-				PathURL pathURL = googlePathURLMap.get(symbol);
-				File file = new File(pathURL.path);
-				if (!file.exists()) {
-					Fetch.download(pathURL.url, pathURL.path, new ArrayList<>());
+				{
+					PathURL pathURL = yahooPathURLMap.get(symbol);
+					File file = new File(pathURL.path);
+					if (!file.exists()) {
+						Fetch.download(pathURL.url, pathURL.path, new ArrayList<>());
+					}
+				}
+				{
+					PathURL pathURL = googlePathURLMap.get(symbol);
+					File file = new File(pathURL.path);
+					if (!file.exists()) {
+						Fetch.download(pathURL.url, pathURL.path, new ArrayList<>());
+					}
 				}
 			}
 		}
@@ -196,52 +154,56 @@ public class UpdatePrice {
 		logger.info("Build map");
 		Map<String, Map<String, PriceTable>> yahooMap  = new HashMap<>();
 		Map<String, Map<String, PriceTable>> googleMap = new HashMap<>();
-		for(String symbol: nasdaqMap.keySet()) {
-			{
-				final String path = yahooPathURLMap.get(symbol).path;
-				final File file = new File(path);
-				if (!file.isFile()) {
-					logger.error("not file  {}", path);
-					throw new SecuritiesException("no file");
+		{
+			final int total = nasdaqMap.keySet().size();
+			int count = 0;
+			for(String symbol: nasdaqMap.keySet()) {
+				if ((count++ % 100) == 0) {
+					logger.info("{}", String.format("%4d / %4d  %s", count, total, symbol));
 				}
-				final Map<String, PriceTable> dateMap = new HashMap<>();
-				yahooMap.put(symbol, dateMap);
-				try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-					final String header = br.readLine();
-					if (header == null) {
-						logger.error("header is null", path);
-						throw new SecuritiesException("header is null");
+
+				{
+					final String path = yahooPathURLMap.get(symbol).path;
+					final File file = new File(path);
+					if (!file.isFile()) {
+						logger.error("not file  {}", path);
+						throw new SecuritiesException("no file");
 					}
-					YahooDaily.CSVRecord.checkHeader(header);
-					for(;;) {
-						final String line = br.readLine();
-						if (line == null) break;
-						final PriceTable priceTable = YahooDaily.CSVRecord.toPriceTable(symbol, line);
-						dateMap.put(priceTable.date, priceTable);
+					final Map<String, PriceTable> dateMap = new HashMap<>();
+					yahooMap.put(symbol, dateMap);
+					try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+						final String header = br.readLine();
+						if (header != null) {
+							YahooDaily.CSVRecord.checkHeader(header);
+							for(;;) {
+								final String line = br.readLine();
+								if (line == null) break;
+								final PriceTable priceTable = YahooDaily.CSVRecord.toPriceTable(symbol, line);
+								dateMap.put(priceTable.date, priceTable);
+							}
+						}
 					}
 				}
-			}
-			{
-				final String path = googlePathURLMap.get(symbol).path;
-				final File file = new File(path);
-				if (!file.isFile()) {
-					logger.error("not file  {}", path);
-					throw new SecuritiesException("no file");
-				}
-				final Map<String, PriceTable> dateMap = new HashMap<>();
-				googleMap.put(symbol, dateMap);
-				try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-					final String header = br.readLine();
-					if (header == null) {
-						logger.error("header is null", path);
-						throw new SecuritiesException("header is null");
+				{
+					final String path = googlePathURLMap.get(symbol).path;
+					final File file = new File(path);
+					if (!file.isFile()) {
+						logger.error("not file  {}", path);
+						throw new SecuritiesException("no file");
 					}
-					GoogleHistorical.CSVRecord.checkHeader(header);
-					for(;;) {
-						final String line = br.readLine();
-						if (line == null) break;
-						final PriceTable priceTable = GoogleHistorical.CSVRecord.toPriceTable(symbol, line);
-						dateMap.put(priceTable.date, priceTable);
+					final Map<String, PriceTable> dateMap = new HashMap<>();
+					googleMap.put(symbol, dateMap);
+					try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+						final String header = br.readLine();
+						if (header != null) {
+							GoogleHistorical.CSVRecord.checkHeader(header);
+							for(;;) {
+								final String line = br.readLine();
+								if (line == null) break;
+								final PriceTable priceTable = GoogleHistorical.CSVRecord.toPriceTable(symbol, line);
+								dateMap.put(priceTable.date, priceTable);
+							}
+						}
 					}
 				}
 			}
@@ -254,49 +216,48 @@ public class UpdatePrice {
 		logger.info("Build data");
 		List<String>     badSymbols = new ArrayList<>();
 		List<PriceTable> data = new ArrayList<>();
-		for(String symbol: badSymbols) {			
-			final Map<String, PriceTable> yahoo  = yahooMap.get(symbol);
-			final Map<String, PriceTable> google = googleMap.get(symbol);
+		{
+			final int total = nasdaqMap.keySet().size();
+			int count = 0;
 
-			int countMissing = 0;
-			for(String date: dateList) {
-				if (yahoo.containsKey(date)) {
-					data.add(yahoo.get(date));
-				} else {
-					if (google.containsKey(date)) {
-						data.add(google.get(date));
+			for(String symbol: nasdaqMap.keySet()) {			
+				if ((count++ % 100) == 0) {
+					logger.info("{}", String.format("%4d / %4d  %s", count, total, symbol));
+				}
+
+				final Map<String, PriceTable> yahoo  = yahooMap.get(symbol);
+				final Map<String, PriceTable> google = googleMap.get(symbol);
+
+				int countMissing = 0;
+				for(String date: dateList) {
+					if (yahoo.containsKey(date)) {
+						data.add(yahoo.get(date));
 					} else {
-						countMissing++;
+						if (google.containsKey(date)) {
+							data.add(google.get(date));
+						} else {
+							countMissing++;
+						}
 					}
 				}
+				if (0 < countMissing) {
+					// repair failed with yahoo and google (some data is still missing)
+					badSymbols.add(symbol);
+				}
 			}
-			if (0 < countMissing) {
-				// repair failed with yahoo and google (some data is still missing)
-				badSymbols.add(symbol);
-			}
+
 		}
 		
-		logger.info("Build data");
-		if (badSymbols.size() == 0) {
-			// Save content of goodData as saveFile
-			final String saveFilePath = String.format("tmp/database/price-%d.csv", dateFrom.getYear());
+		logger.info("Save data");
+		{
+			final String saveFilePathFormat = (badSymbols.size() == 0) ? "tmp/database/price-%d.csv" : "tmp/database/price-%d.BAD";
+			final String saveFilePath = String.format(saveFilePathFormat, dateFrom.getYear());
 			final File saveFile = new File(saveFilePath);
 			if (!saveFile.exists()) saveFile.createNewFile();
 			
 			logger.info("Save {}", saveFilePath);
 			saveFile.createNewFile();
 			try (BufferedWriter save = new BufferedWriter(new FileWriter(saveFile))) {
-				for(PriceTable table: data) {
-					save.append(table.toCSV()).append("\n");
-				}
-			}
-		} else {
-			// Save content of goodData as bad saveFile
-			final String badSaveFilePath = String.format("tmp/database/price-%d.BAD", dateFrom.getYear());
-			logger.info("Save {}  {}", badSaveFilePath,  badSymbols.size());
-			File badSaveFile = new File(badSaveFilePath);
-			if (!badSaveFile.exists()) badSaveFile.createNewFile();
-			try (BufferedWriter save = new BufferedWriter(new FileWriter(badSaveFile))) {
 				for(PriceTable table: data) {
 					save.append(table.toCSV()).append("\n");
 				}

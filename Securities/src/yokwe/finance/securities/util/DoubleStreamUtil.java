bab5cfd155a4c9f134dac998e27a3ecf5ad0b901
yokwe <yokew@github.com>
2015-11-10 11:21:17 +0900

    Add MovingAverage and Sample class.

--- Securities/src/yokwe/finance/securities/util/DoubleStreamUtil.java
+++ Securities/src/yokwe/finance/securities/util/DoubleStreamUtil.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.DoubleConsumer;
 import java.util.function.DoubleFunction;
+import java.util.function.DoubleUnaryOperator;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.slf4j.Logger;
@@ -45,7 +47,6 @@ public class DoubleStreamUtil {
 	public static final class MovingStats {
 		public static class MapToObj implements DoubleFunction<MovingStats> {
 			final DescriptiveStatistics stats;
-			boolean firstTime = true;
 			public MapToObj(int interval) {
 				stats = new DescriptiveStatistics(interval);
 			}
@@ -81,7 +82,67 @@ public class DoubleStreamUtil {
 			this.skewness = skewness;
 		}
 	}
+
+	public static final class Sample implements DoubleFunction<DoubleStream> {
+		private int interval;
+		private int count;
+		private Sample(int interval) {
+			this.interval = interval;
+			this.count    = 0;
+		}
+
+		@Override
+		public DoubleStream apply(double value) {
+			count++;
+			if (count == interval) {
+				count = 0;
+				return Arrays.stream(new double[]{value});
+			} else {
+				return null;
+			}
+		}
+		public static Sample getInstance(int interval) {
+			return new Sample(interval);
+		}
+	}
 	
+	private static void testSampling() {
+		double[] values = {
+				1, 2, 3,  4,  5,  6,
+				7, 8, 9, 10, 11, 12,
+		};
+		
+		double[] smaple3 = Arrays.stream(values).flatMap(Sample.getInstance(3)).toArray();
+		double[] smaple6 = Arrays.stream(values).flatMap(Sample.getInstance(6)).toArray();
+		
+		logger.info("values   {}", Arrays.stream(values).boxed().collect(Collectors.toList()));
+		logger.info("sample 3 {}", Arrays.stream(smaple3).boxed().collect(Collectors.toList()));
+		logger.info("sample 6 {}", Arrays.stream(smaple6).boxed().collect(Collectors.toList()));
+	}
+
+	public static final class MovingAverage implements DoubleUnaryOperator {
+		final DescriptiveStatistics stats;
+		
+		private MovingAverage(int interval) {
+			this.stats = new DescriptiveStatistics(interval);
+		}
+
+		@Override
+		public double applyAsDouble(double value) {
+			if (stats.getN() == 0) {
+				for(int i = 1; i < stats.getWindowSize(); i++) {
+					stats.addValue(value);
+				}
+			}
+			stats.addValue(value);
+			return stats.getMean();
+		}
+
+		public static MovingAverage getInstance(int interval) {
+			return new MovingAverage(interval);
+		}
+	}
+
 	
 	private static void testMovingStats() {
 		double[] values = {
@@ -109,6 +170,21 @@ public class DoubleStreamUtil {
 		}
 	}
 	
+	private static void testMovingAverage() {
+		double[] values = {
+				1, 1, 1,   2, 2, 2,
+				3, 3, 3,   4, 4, 4,
+				5, 5, 5,   6, 6, 6,
+		};
+		
+		double[] ma3 = Arrays.stream(values).map(MovingAverage.getInstance(3)).toArray();
+		double[] ma6 = Arrays.stream(values).map(MovingAverage.getInstance(6)).toArray();
+		
+		logger.info("values {}", Arrays.stream(values).boxed().collect(Collectors.toList()));
+		logger.info("ma 3   {}", Arrays.stream(ma3).boxed().collect(Collectors.toList()));
+		logger.info("ma 6   {}", Arrays.stream(ma6).boxed().collect(Collectors.toList()));
+	}
+	
 	private static void testStats() {
 		{
 			double[] values = {
@@ -134,5 +210,7 @@ public class DoubleStreamUtil {
 	public static void main(String[] args) {
 		testMovingStats();
 		testStats();
+		testSampling();
+		testMovingAverage();
 	}
 }

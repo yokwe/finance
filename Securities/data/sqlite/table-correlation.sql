ff46ce6b1fccb350475d5cbd06222843f18065d3
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-11-12 08:09:25 +0900

    Add select statement to show time.

--- Securities/data/sqlite/table-correlation.sql
+++ Securities/data/sqlite/table-correlation.sql
@@ -20,19 +20,33 @@ CREATE TABLE correlation (
 
 .separator ,
 
+select time('now', 'localtime');
 .import tmp/database/correlation-03.csv   correlation
+select time('now', 'localtime');
 .import tmp/database/correlation-12.csv   correlation
+select time('now', 'localtime');
 .import tmp/database/correlation-36.csv   correlation
+select time('now', 'localtime');
 .import tmp/database/correlation-60.csv   correlation
+select time('now', 'localtime');
 
 CREATE        INDEX correlation_month           ON correlation(month);
+select time('now', 'localtime');
 CREATE        INDEX correlation_a               ON correlation(a);
+select time('now', 'localtime');
 CREATE        INDEX correlation_b               ON correlation(b);
+select time('now', 'localtime');
 CREATE        INDEX correlation_cc              ON correlation(cc);
+select time('now', 'localtime');
 
 select count(*)              from correlation;
+select time('now', 'localtime');
 select count(distinct month) from correlation;
+select time('now', 'localtime');
 select count(distinct a)     from correlation;
+select time('now', 'localtime');
 select count(distinct b)     from correlation;
+select time('now', 'localtime');
 
 select month, count(*)       from correlation group by month;
+select time('now', 'localtime');
--- Securities/data/sqlite/table-create.sql
+++ Securities/data/sqlite/table-create.sql
@@ -41,32 +41,51 @@ CREATE TABLE dividend (
 .tables
 
 .separator ,
+select time('now', 'localtime');
 
 .import tmp/database/nasdaq.csv           nasdaq
+select time('now', 'localtime');
 .import tmp/database/nasdaq-company.csv   company
+select time('now', 'localtime');
 
 -- .import tmp/database/yahoo-dividend.csv   dividend
 .import tmp/database/dividend-all.csv     dividend
+select time('now', 'localtime');
 
 -- .import tmp/database/yahoo-daily.csv      price
 -- .import tmp/database/google-getprices.csv price
 .import tmp/database/price-all.csv        price
+select time('now', 'localtime');
 
 CREATE        INDEX nasdaq_etf           ON nasdaq(etf);
+select time('now', 'localtime');
 CREATE UNIQUE INDEX nasdaq_symbol        ON nasdaq(symbol);
+select time('now', 'localtime');
 
 CREATE UNIQUE INDEX company_symbol       ON company(symbol);
+select time('now', 'localtime');
 
 CREATE        INDEX dividend_date        ON dividend(date);
+select time('now', 'localtime');
 CREATE        INDEX dividend_symbol      ON dividend(symbol);
+select time('now', 'localtime');
 CREATE UNIQUE INDEX dividend_symbol_date ON dividend(symbol, date);
+select time('now', 'localtime');
 
 CREATE        INDEX price_date           ON price(date);
+select time('now', 'localtime');
 CREATE        INDEX price_symbol         ON price(symbol);
+select time('now', 'localtime');
 CREATE UNIQUE INDEX price_symbol_date    ON price(symbol, date);
+select time('now', 'localtime');
 
 select count(*) from nasdaq;
+select time('now', 'localtime');
 select count(*) from company;
+select time('now', 'localtime');
 select count(*) from price;
+select time('now', 'localtime');
 select count(*) from dividend;
+select time('now', 'localtime');
 select date, count(*) from price group by date order by date desc limit 5;
+select time('now', 'localtime');

d6f53c31aa3bc2746fe7f5c98164175c9a74d936
yokwe <yokew@github.com>
2015-11-11 14:38:22 +0900

    Reduce log message.

--- Securities/src/yokwe/finance/securities/app/FindCorrelation.java
+++ Securities/src/yokwe/finance/securities/app/FindCorrelation.java
@@ -3,15 +3,12 @@ package yokwe.finance.securities.app;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -19,13 +16,12 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import yokwe.finance.securities.SecuritiesException;
-import yokwe.finance.securities.database.NasdaqTable;
 import yokwe.finance.securities.database.PriceTable;
+import yokwe.finance.securities.util.Correlation;
 import yokwe.finance.securities.util.DoubleStreamUtil.MovingAverage;
 import yokwe.finance.securities.util.DoubleStreamUtil.Sample;
 
@@ -33,134 +29,49 @@ public final class FindCorrelation {
 	private static final Logger logger = LoggerFactory.getLogger(FindCorrelation.class);
 
 	private static final String JDBC_URL    = "jdbc:sqlite:tmp/sqlite/securities.sqlite3";
-	private static final String OUTPUT_PATH = "tmp/correlaton.csv";
+	private static final String OUTPUT_PATH = "tmp/database/correlaton-%d.csv";
 	
 //	private static final int MIN_SAMPLE_COUNT =  80;
 //	private static final int MAX_SAMPLE_COUNT = 120;
 	
-	private static final int MIN_SAMPLE_COUNT = 480;
-	private static final int MAX_SAMPLE_COUNT = 520;
+	private static final int MIN_SAMPLE_COUNT = 400;
+	private static final int MAX_SAMPLE_COUNT = 600;
 	
 //	private static final int MIN_SAMPLE_COUNT =  980;
 //	private static final int MAX_SAMPLE_COUNT = 1020;
 	
-	static final class CorrelationMap {
-		public  final int                   length;
-		public  final int                   size;
-		private final Map<String, double[]> dataMap;
-		private final Map<String, Integer>  indexMap; // map from name to index
-		private final String[]              names;    // all name in dataMap
-		private final double[][]            ccArray;  // double[size][size]
+	private static final int DURATION_DAYS = 90;
+	private static final int MIN_AVG_VOL   = 50_000;
+	
+	private static LocalDate getTradingDate(Connection connection, final LocalDate date) {
+		LocalDate tradingDate = date.minusDays(0);
+		if (PriceTable.isTradingDay(connection, tradingDate)) return tradingDate;
+		tradingDate = tradingDate.minusDays(1);
+		if (PriceTable.isTradingDay(connection, tradingDate)) return tradingDate;
+		tradingDate = tradingDate.minusDays(1);
+		if (PriceTable.isTradingDay(connection, tradingDate)) return tradingDate;
+		tradingDate = tradingDate.minusDays(1);
+		if (PriceTable.isTradingDay(connection, tradingDate)) return tradingDate;
 		
-		CorrelationMap(Map<String, double[]> doubleMap) {
-			length   = doubleMap.get("IBM").length;
-			size     = doubleMap.size();
-
-			// Sanity check
-			for(String name: doubleMap.keySet()) {
-				double[] data = doubleMap.get(name);
-				if (data.length != length) {
-					logger.error("name = {}  length = {}  data.length = {}", name, length, data.length);
-					throw new SecuritiesException("length");
-				}
-			}
-
-			dataMap  = new HashMap<>(doubleMap);
-			indexMap = new HashMap<>();
-			names    = new String[size];
-			ccArray  = new double[size][size];
-
-			double[][] devArray = new double[size][length];
-			double[]   sdArray  = new double[size];
-			
-			{
-				int index = 0;
-				for(String name: dataMap.keySet()) {
-					names[index++] = name;
-				}
-				// Sort names before use.  dataMap can be HashMap
-				Arrays.sort(names);
-			}
-			
-			for(int index = 0; index < names.length; index++) {
-				final String name = names[index];
-				indexMap.put(name, index);
-				
-				final double[] data = dataMap.get(name);
-				final double[] dev  = devArray[index];
-				
-				double mean = 0;
-				for(double e: data) mean += e;
-				mean /= length;
-				
-				double var = 0;
-				for(int i = 0; i < length; i++) {
-					double t = data[i] - mean;
-					dev[i] = t;
-					var += t * t;
-				}
-				sdArray[index] = 1.0 / Math.sqrt(var);
-			}
-			// Compute ccArray from devArray and sdArray
-			for(int x = 0; x < size; x++) {
-				for(int y = 0; y < x; y++) {
-					double[] dataX = devArray[x];
-					double[] dataY = devArray[y];
-					double   sdX   = sdArray[x];
-					double   sdY   = sdArray[y];
-					
-					double cc = 0;
-					for(int i = 0; i < dataX.length; i++) cc += dataX[i] * dataY[i];
-					cc *= (sdX * sdY);
-					ccArray[x][y] = cc;
-					ccArray[y][x] = cc;
-				}
-				ccArray[x][x] = 1.0;
-			}
-		}
-		private int getIndex(String name) {
-			if (indexMap.containsKey(name)) {
-				return indexMap.get(name);
-			} else {
-				logger.error("Unknown name = {}", name);
-				throw new SecuritiesException("unknown name");
-			}
-		}
-		double getCorrelation(String nameX, String nameY) {
-			final int indexX = getIndex(nameX);
-			final int indexY = getIndex(nameY);
-			
-			return ccArray[indexX][indexY];
-		}
-		
-		double getCorrelationX(String nameX, String nameY) {
-			double[] x = dataMap.get(nameX);
-			double[] y = dataMap.get(nameY);
-			return new PearsonsCorrelation().correlation(x, y);
-		}
-		Map<String, Double> getCorrelation(String name) {
-			final int index = getIndex(name);
-			Map<String, Double> ret = new TreeMap<>();
-			for(int i = 0; i < names.length; i++) {
-				ret.put(names[i], ccArray[index][i]);
-			}
-			return ret;
-		}
+		logger.error("date = {}  tradingDate = {}", date, tradingDate);
+		throw new SecuritiesException("tradingDate");
 	}
 	
-	static void calculate(final Connection connection, final Writer w, final int months) throws IOException {
+	static void calculate(final Connection connection, final int months) throws IOException {
 		logger.info("month         = {}", months);
 
 		// dateFrom and dateTo is range of days (both inclusive)
 		LocalDate dateTo   = LocalDate.parse(PriceTable.getLastTradeDate(connection));;
 		LocalDate dateFrom = dateTo.minusMonths(months).plusDays(1);
-		logger.info("dateRange     = {} - {}", dateFrom, dateTo);		
+		if (!PriceTable.isTradingDay(connection, dateFrom)) {
+			dateFrom = getTradingDate(connection, dateFrom);
+		}
+//		logger.info("dateRange     = {} - {}", dateFrom, dateTo);		
 		
-		Map<String, NasdaqTable> nasdaqMap = NasdaqTable.getMap(connection);
-		logger.info("nasdaqMap     = {}", nasdaqMap.size());
-
 		List<PriceTable> dataListTo   = PriceTable.getAllByDate(connection, dateTo);
 		List<PriceTable> dataListFrom = PriceTable.getAllByDate(connection, dateFrom);
+//		logger.info("dataListTo    = {}", dataListTo.size());
+//		logger.info("dataListFrom  = {}", dataListFrom.size());
 		
 		// symbolList has all symbols
 		List<String> symbolList;
@@ -174,7 +85,7 @@ public final class FindCorrelation {
 			symbolList = new ArrayList<>(set);
 			Collections.sort(symbolList);
 		}
-		logger.info("symbolList    = {}", symbolList.size());
+//		logger.info("symbolList    = {}", symbolList.size());
 		
 		// filter symbol
 		{
@@ -187,29 +98,25 @@ public final class FindCorrelation {
 			
 			symbolList = newSymbolList;
 		}
-		logger.info("symbolList    = {}", symbolList.size());
+//		logger.info("symbolList    = {}", symbolList.size());
 		
-//		// Filter out securities that has average volume for last 90 days is less than 50,000
-//		{
-//			List<String> newSymbolList = new ArrayList<>();
-//
-//			final int DURATION_DAYS = 90;
-//			final int MIN_AVG_VOL   = 50_000;
-//			logger.info("DURATION_DAYS = {}", DURATION_DAYS);
-//			logger.info("MIN_AVG_VOL   = {}", MIN_AVG_VOL);
-//			
-//			for(String symbol: symbolList) {
-//				double averageVolume = PriceTable.getAverageVolume(connection, symbol, dateFrom, dateTo);
-//				if (averageVolume < MIN_AVG_VOL) continue;
-//				
-//				newSymbolList.add(symbol);
-//			}
-//			symbolList = newSymbolList;
-//		}
+		{
+//			logger.info("Filter by average volume", symbolList.size());
+			LocalDate dateFromFilter = dateTo.minusDays(DURATION_DAYS);
+			List<String> newSymbolList = new ArrayList<>();
+
+			for(String symbol: symbolList) {
+				double averageVolume = PriceTable.getAverageVolume(connection, symbol, dateFromFilter, dateTo);
+				if (averageVolume < MIN_AVG_VOL) continue;
+				
+				newSymbolList.add(symbol);
+			}
+			symbolList = newSymbolList;
+		}
 //		logger.info("symbolList    = {}", symbolList.size());
 		
 		List<String> dateList = PriceTable.getAllBySymbolDateRange(connection, "IBM", dateFrom, dateTo).stream().map(o -> o.date).collect(Collectors.toList());
-		logger.info("dateList      = {}", dateList.size());
+//		logger.info("dateList      = {}", dateList.size());
 		final int totalCount = dateList.size();
 		int sample = -1;
 		int skip   = totalCount;
@@ -238,7 +145,6 @@ public final class FindCorrelation {
 				skip   = 0;
 			}
 		}
-		logger.info("sample count  {}  -  {}", MIN_SAMPLE_COUNT, MAX_SAMPLE_COUNT);
 		logger.info("totalCount    {}  =  {} (sample) *  {} (count)  +  {} (skip)", totalCount, sample, totalCount / sample, skip);
 		
 		Map<String, double[]> priceMap = new TreeMap<>();
@@ -253,34 +159,45 @@ public final class FindCorrelation {
 		}
 		
 		{
-			logger.info("CorrelationMap");
-			CorrelationMap cm = new CorrelationMap(priceMap);
-			logger.info("cm   {} x {}", cm.size, cm.length);
+//			logger.info("Correlation");
+			Correlation correlation = new Correlation(priceMap);
+			logger.info("correation   {} x {}", correlation.size, correlation.length);
 			
 			//  SPY  QQQ = 0.980560447
-			logger.info("X SPY  QQQ = {}", String.format("%.9f", cm.getCorrelationX("SPY", "QQQ")));
-			logger.info("  SPY  QQQ = {}", String.format("%.9f", cm.getCorrelation("SPY", "QQQ")));
-			{
-				Map<String, Double> map = cm.getCorrelation("SPY");
-				logger.info("  SPY  QQQ = {}", String.format("%.9f", map.get("QQQ")));
-				for(String key: map.keySet()) {
-					double value = map.get(key);
-					if (value < -0.5) {
-						logger.info("{}", String.format("%-6s  %-6s  %6.2f", "SPY", key, value));
+			logger.info("X SPY  QQQ = {}", String.format("%.9f", correlation.getCorrelationX("SPY", "QQQ")));
+			logger.info("  SPY  QQQ = {}", String.format("%.9f", correlation.getCorrelation("SPY", "QQQ")));
+			
+			String csvPath = String.format(OUTPUT_PATH, months);
+			logger.info("OUTPUT CSV START  {}", csvPath);
+			try (BufferedWriter csv = new BufferedWriter(new FileWriter(csvPath))) {
+				for(String symbolA: correlation.getNames()) {
+					Map<String, Double> map = correlation.getCorrelation(symbolA);
+					
+					for(String symbolB: map.keySet()) {
+						double v = map.get(symbolB);
+						// symbolA, symbolB, correlation
+						csv.append(String.format("%s,%s,%.2f\n", symbolA, symbolB, v));
 					}
 				}
 			}
+			logger.info("OUTPUT CSV STOP");
 		}
 	}
 	
 	public static void main(String[] args) {
 		logger.info("START");
-		final int months = 48;
+		logger.info("DURATION_DAYS    = {}", DURATION_DAYS);
+		logger.info("MIN_AVG_VOL      = {}", MIN_AVG_VOL);
+		logger.info("MIN_SAMPLE_COUNT = {}", MIN_SAMPLE_COUNT);
+		logger.info("MAX_SAMPLE_COUNT = {}", MAX_SAMPLE_COUNT);
+		
 		try {
 			Class.forName("org.sqlite.JDBC");
-			try (Connection connection = DriverManager.getConnection(JDBC_URL);
-				BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_PATH))) {
-				calculate(connection, bw, months);
+			try (Connection connection = DriverManager.getConnection(JDBC_URL)) {
+				calculate(connection, 3);
+				calculate(connection, 12);
+				calculate(connection, 36);
+				calculate(connection, 60);
 			}
 		} catch (ClassNotFoundException | SQLException | IOException e) {
 			logger.error(e.getClass().getName());

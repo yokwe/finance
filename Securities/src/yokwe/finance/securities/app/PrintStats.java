d2b85c66f0d8895f467ce829acf8266f62d312aa
hasegawa <hasegawa.yasuhiro@gmail.com>
2015-12-01 21:40:31 +0900

    Use table name as method name.

--- Securities/src/yokwe/finance/securities/app/PrintStats.java
+++ Securities/src/yokwe/finance/securities/app/PrintStats.java
@@ -9,48 +9,15 @@ public class PrintStats {
 	static double CONFIDENCE_95 = 1.65;
 	static double CONFIDENCE_99 = 2.33;
 	
-	static final double mean(double[] data) {
-		final int LENGTH = data.length;
-		double ret = 0;
-		for(int i = 0; i < LENGTH; i++) {
-			ret += data[i];
-		}
-		return ret / LENGTH;
-	}
-	
-	static double sd_equal(double[] data) {
-		final double mean = mean(data);
-		final int LENGTH = data.length;
-		double ret = 0;
-		for(int i = 0; i < LENGTH; i++) {
-			final double d = data[i] - mean;
-			ret += d * d;
-		}
-		return Math.sqrt(ret / LENGTH);
-	}
-	// 0 < lambda < 1
-	static double sd_exp(double[] data, final double lambda) {
-		final double mean = mean(data);
-		final int LENGTH = data.length;
-		double ret = 0;
-		double lambda_exp = 1;
-		for(int i = 0; i < LENGTH; i++) {
-			final double d = data[i] - mean;
-			ret += lambda_exp * d * d;
-			lambda_exp *= lambda;
-		}
-		return Math.sqrt((1 - lambda) * ret);
-	}
-	
-	
 	public static void main(String[] args) {
 		logger.info("START");
-		print1();
-		print2();
+		table_4_1();
+		table_5_2();
+		table_5_3();
 		logger.info("STOP");
 	}
 	
-	private static void print1() {
+	private static void table_4_1() {
 		final double[] data = {
 				0.67654,
 				0.67732,
@@ -67,6 +34,7 @@ public class PrintStats {
 		};
 
 
+		logger.info("table 4.1");
 		for(int i = 1; i < data.length; i++) {
 			double P0 = data[i - 1];
 			double P1 = data[i];
@@ -78,11 +46,11 @@ public class PrintStats {
 			// log price change or continuously compound return
 			double r1 = Math.log(P1 / P0) * 100;
 			
-			logger.info("print1 {}", String.format("%8.5f  %8.3f  %8.3f  %8.3f", P1, D1, R1, r1));
+			logger.info("table 4.1  {}", String.format("%8.5f  %8.3f  %8.3f  %8.3f", P1, D1, R1, r1));
 		}
 	}
 	
-	private static void print2() {
+	private static void table_5_2() {
 		final double[] data = {
 				 0.634,
 				 0.115,
@@ -105,21 +73,62 @@ public class PrintStats {
 				-0.105,
 				-0.257,
 				};
-
-		logger.info("");
-		for(int i = 1; i < data.length; i++) {
-			double P0 = data[i - 1];
-			double P1 = data[i];
-			// absolute price change
-			double D1 = (P1 - P0) * 100;
-			// price percent change or price return
-			double R1 = D1 / P0;
-//			double r1 = (Math.log(P1) - Math.log(P0)) * 100;
-			// log price change or continuously compound return
-			double r1 = Math.log(P1 / P0) * 100;
-
-			logger.info("print2 {}", String.format("%8.5f  %8.3f  %8.3f  %8.3f", P1, D1, R1, r1));
+		final int T = data.length;
+		final double DECAY_FACTOR = 0.94;
+		
+		final double expTable[] = new double[T];
+		{
+			double t = 1.0;
+			for(int i = 0; i < T; i++) {
+				expTable[T - i - 1] = (1 - DECAY_FACTOR) * t;
+				t *= DECAY_FACTOR;
+			}
 		}
 
+		logger.info("table 5.2");
+		for(int i = 0; i < T; i++) {
+			double a = data[i];
+			double b = a * a;
+			double c = 1.0 / T;
+			double d = expTable[i];
+
+			logger.info("table 5.2  {}", String.format("%8.5f  %8.3f  %8.3f  %8.3f  %8.3f  %8.3f", a, b, c, d, b * c, b * d));
+		}
+	}
+	
+	static void table_5_3() {
+		final double[] data = {
+				 0.633,
+				 0.115,
+				-0.459,
+				 0.093,
+				 0.176,
+				-0.087,
+				-0.142,
+				 0.324,
+				-0.943,
+				-0.528,
+				-0.107,
+				-0.159,
+				-0.445,
+			 	 0.053,
+				 0.152,
+				-0.318,
+				 0.424,
+				-0.708,
+				-0.105,
+				-0.257,
+				};
+		final int T = data.length;
+		final double DECAY_FACTOR = 0.94;
+		
+		logger.info("table 5.3");
+		double b = data[0] * data[0];
+		for(int i = 0; i < T; i++) {
+			double a = data[i];
+			b = (DECAY_FACTOR * b) + (1 - DECAY_FACTOR) * (a * a);
+
+			logger.info("table 5.3  {}", String.format("%8.3f  %8.3f", a, b));
+		}
 	}
 }

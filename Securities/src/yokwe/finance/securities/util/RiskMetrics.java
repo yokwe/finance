7d5e09e4a39a15e91988c471887a570b5bfe8b6a
yokwe <yokew@github.com>
2015-12-03 15:04:30 +0900

    Refactor structure of RiskMetrics.

--- Securities/src/yokwe/finance/securities/util/RiskMetrics.java
+++ Securities/src/yokwe/finance/securities/util/RiskMetrics.java
@@ -1,5 +1,8 @@
 package yokwe.finance.securities.util;
 
+import java.util.Arrays;
+import java.util.function.DoubleUnaryOperator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,133 +15,187 @@ public final class RiskMetrics {
 	public static double DEFAULT_DECAY_FACTOR = 0.94;
 	public static double DEFAULT_CONFIDENCE   = CONFIDENCE_95_PERCENT;
 	
-	final double   decayFactor;
+	private static final DoubleUnaryOperator APPLY_SQRT = new DoubleUnaryOperator() {
+		@Override
+		public double applyAsDouble(double value) {
+			return Math.sqrt(value);
+		}
+	};
+	private static DoubleUnaryOperator applySqrt() {
+		return APPLY_SQRT;
+	}
+	
+	private static final class RecursiveVariance implements DoubleUnaryOperator {
+		private final double decayFactor;
+		private double var = Double.NaN;
+		
+		RecursiveVariance(double decayFactor) {
+			this.decayFactor = decayFactor;
+		}
+		@Override
+		public double applyAsDouble(double value) {
+			if (Double.isNaN(var)) {
+				var = value;
+			}
+			var = value + decayFactor * (var - value);
+			return var;
+		}
+	};
+	private DoubleUnaryOperator recursiveVariance() {
+		return new RecursiveVariance(decayFactor);
+	}
+	
+	public static double[] multiply(double a[], double b[]) {
+		double ret[] = new double[a.length];
+		for(int i = 0; i < a.length; i++) {
+			ret[i] = a[i] * b[i];
+		}
+		return ret;
+	}
+	public static double[] divide(double a[], double b[]) {
+		double ret[] = new double[a.length];
+		for(int i = 0; i < a.length; i++) {
+			ret[i] = a[i] / b[i];
+		}
+		return ret;
+	}
+	
+	
+	final double              decayFactor;
+	final DoubleUnaryOperator recursiveVariance;
 	
 	public RiskMetrics(double decayFactor) {
-		this.decayFactor = decayFactor;
+		this.decayFactor       = decayFactor;
+		this.recursiveVariance = new RecursiveVariance(decayFactor);
 	}
 	public RiskMetrics() {
 		this(DEFAULT_DECAY_FACTOR);
 	}
 	
+	
+	public static double[] getRelativeReturn(double data[]) {
+		double ret[] = new double[data.length - 1];
+		for(int i = 1; i < data.length; i++) {
+			final double d0 = data[i - 1];
+			final double d1 = data[i];
+			ret[i - 1] = ((d1 / d0) - 1) * 100;
+		}
+		return ret;
+	}
+		
 	public static double[] getLogReturn(double data[]) {
-		double ret[] = new double[data.length];
-		for(int i = 0; i < ret.length; i++) {
-			ret[i] = Math.log(data[i] / data[i - 1]);
+		double ret[] = new double[data.length - 1];
+		for(int i = 1; i < data.length; i++) {
+			final double d0 = data[i - 1];
+			final double d1 = data[i];
+			ret[i - 1] = Math.log(d1 / d0) * 100;
 		}
 		return ret;
 	}
-	
-	public static double getEMA(double ema, double value, double decayFactor) {
-		return ema + decayFactor * (value - ema);
-	}
-	
-	public static double getVAR_EWMA(double var, double value, double decayFactor) {
-		// Assume sampled mean of value is zero
-		return value + decayFactor * (var - value);
-	}
-	// This is a variance. So to get Standard Deviation, apply square root to it.
-	public static double[] getVariance(double data[], double decayFactor) {
-		double ret[] = new double[data.length];
 		
-		double var = data[0] * data[0];
-		for(int i = 0; i < ret.length; i++) {
-			final double value = data[i] * data[i];
-			var = getVAR_EWMA(var, value, decayFactor);
-			ret[i] = var;
-		}
-		return ret;
-	}
-	
-	public static double[] getCovariance(double data1[], double data2[], double decayFactor) {
-		double ret[] = new double[data1.length];
-		
-		double cova = data1[0] * data2[0];
-		for(int i = 0; i < data1.length; i++) {
-			double value = data1[i] * data2[i];
-			cova = getVAR_EWMA(cova, value, decayFactor);
-			ret[i] = cova;
-		}
-		return ret;
-	}
-	
-	public static double[] getCorrelation(double data1[], double data2[], double decayFactor) {
-		double var1[] = getVariance(data1, decayFactor);
-		double var2[] = getVariance(data2, decayFactor);
-		double cova[] = getCovariance(data1, data2, decayFactor);
-		
-		double ret[] = new double[data1.length];
-		for(int i = 0; i < ret.length; i++) {
-			ret[i] = cova[i] / (Math.sqrt(var1[i]) * Math.sqrt(var2[i]));
-		}
-		return ret;
-	}
-	
-	public double[] getVariance(double data[]) {
-		return getVariance(data, decayFactor);
-	}
-	public double[] getCovariance(double data1[], double data2[]) {
-		return getCovariance(data1, data2, decayFactor);
-	}
 	public double[] getCorrelation(double data1[], double data2[]) {
-		return getCorrelation(data1, data2, decayFactor);
+		double sd1[] = getStandardDeviation(data1);
+		double sd2[] = getStandardDeviation(data2);
+		double cov[] = getCovariance(data1, data2);
+		
+		return divide(cov, multiply(sd1, sd2));
+	}
+	
+	public double[] getCovariance(double data1[], double data2[]) {
+		return Arrays.stream(multiply(data1, data2)).map(recursiveVariance()).toArray();
+	}
+	public double[] getVariance(double data[]) {
+		return getCovariance(data, data);
+	}
+	public double[] getStandardDeviation(double data[]) {
+		return Arrays.stream(multiply(data, data)).map(recursiveVariance()).map(applySqrt()).toArray();
 	}
 
 	public static void main(String args[]) {
-		final double[] usddem_data = {
-			 0.634,
-			 0.115,
-			-0.460,
-			 0.094,
-			 0.176,
-			-0.088,
-			-0.142,
-			 0.324,
-			-0.943,
-			-0.528,
-			-0.107,
-			-0.160,
-			-0.445,
-		 	 0.053,
-			 0.152,
-			-0.318,
-			 0.424,
-			-0.708,
-			-0.105,
-			-0.257,
+		// Difference between relative return and log return
+		{
+			double[] data = {
+				0.67654,
+				0.67732,
+				0.67422,
+				0.67485,
+				0.67604,
+				0.67545,
+				0.67449,
+				0.67668,
+				0.67033,
+				0.66680,
+				0.66609,
+				0.66503,
 			};
-	
-		double sp500_data[] = {
-			  0.005,
-			 -0.532,
-			  1.267,
-			  0.234,
-			  0.095,
-			 -0.003,
-			 -0.144,
-			 -1.643,
-			 -0.319,
-			 -1.362,
-			 -0.367,
-			  0.872,
-			  0.904,
-			  0.390,
-			 -0.527,
-			  0.311,
-			  0.227,
-			  0.436,
-			  0.568,
-			 -0.217,
-		};
-		
-		RiskMetrics rm = new RiskMetrics();
-		double usddem_var[] = rm.getVariance(usddem_data);
-		double sp500_var[] = rm.getVariance(sp500_data);
-		double cova[] = rm.getCovariance(usddem_data, sp500_data);
-		double corr[] = rm.getCorrelation(usddem_data, sp500_data);
-		
-		for(int i = 0; i < usddem_data.length; i++) {
-			logger.info("{}", String.format("%8.3f  %8.3f  %8.3f  %8.3f  %8.3f  %8.3f", usddem_data[i], sp500_data[i], usddem_var[i], sp500_var[i], cova[i], corr[i]));
+			
+			double rr[] = getRelativeReturn(data);
+			double lr[] = getLogReturn(data);
+			
+			logger.info("");
+			for(int i = 0; i < rr.length; i++) {
+				logger.info("{}", String.format("%8.3f  %8.3f  %8.3f", data[i + 1], rr[i], lr[i]));
+			}
 		}
+		
+		{
+			double[] data_a = {
+				 0.634,
+				 0.115,
+				-0.460,
+				 0.094,
+				 0.176,
+				-0.088,
+				-0.142,
+				 0.324,
+				-0.943,
+				-0.528,
+				-0.107,
+				-0.160,
+				-0.445,
+			 	 0.053,
+				 0.152,
+				-0.318,
+				 0.424,
+				-0.708,
+				-0.105,
+				-0.257,
+			};
+
+			double data_b[] = {
+				  0.005,
+				 -0.532,
+				  1.267,
+				  0.234,
+				  0.095,
+				 -0.003,
+				 -0.144,
+				 -1.643,
+				 -0.319,
+				 -1.362,
+				 -0.367,
+				  0.872,
+				  0.904,
+				  0.390,
+				 -0.527,
+				  0.311,
+				  0.227,
+				  0.436,
+				  0.568,
+				 -0.217,
+			};
+
+			RiskMetrics rm = new RiskMetrics();
+			double rva[] = rm.getVariance(data_a);
+			double rvb[] = rm.getVariance(data_b);
+			double cov[] = rm.getCovariance(data_a, data_b);
+			double cor[] = rm.getCorrelation(data_a, data_b);
+			
+			logger.info("");
+			for(int i = 0; i < data_a.length; i++) {
+				logger.info("{}", String.format("%8.3f  %8.3f  %8.3f  %8.3f  %8.3f  %8.3f", data_a[i], data_b[i], rva[i], rvb[i], cov[i], cor[i]));
+			}
+		}
+
 	}
 }

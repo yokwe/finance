c2d67e7e9beefb2b93367bf5f4d2d80c84a0eeb0
yokwe <yokew@github.com>
2015-12-11 10:57:03 +0900

    Use different algorithm (code) to improve correctness.

--- Securities/src/yokwe/finance/securities/stats/DoubleArray.java
+++ Securities/src/yokwe/finance/securities/stats/DoubleArray.java
@@ -126,35 +126,32 @@ public final class DoubleArray {
 		}
 		
 		public BiStats(double data1[], double data2[]) {
-//			if (data1.length != data2.length) {
-//				logger.error("data1.length = {}  data2.length = {}", data1.length, data2.length);
-//				throw new SecuritiesException("data1.length != data2.length");
-//			}
-//			final int size = data1.length;
-//			
-//			double mean1 = mean(data1);
-//			double mean2 = mean(data2);
-//			double cov   = 0;
-//			double var1  = 0;
-//			double var2  = 0;
-//			double diff1[] = new double[size];
-//			double diff2[] = new double[size];
-//			for(int i = 0; i < size; i++) {
-//				double d1 = data1[i] - mean1;
-//				double d2 = data2[i] - mean2;
-//				cov  += d1 * d2;
-//				var1 += d1 * d1;
-//				var2 += d2 * d2;
-//				diff1[i] = d1;
-//				diff2[i] = d2;
-//			}
-//			correlation = cov / (Math.sqrt(var1) * Math.sqrt(var2));
-//			covariance  = cov /size;
-//			var1 /= size;
-//			var2 /= size;
-//			stats1 = new UniStats(size, mean1, var1, diff1);
-//			stats2 = new UniStats(size, mean2, var2, diff2);
-			this(new UniStats(data1), new UniStats(data2));
+			if (data1.length != data2.length) {
+				logger.error("data1.length = {}  data2.length = {}", data1.length, data2.length);
+				throw new SecuritiesException("data1.length != data2.length");
+			}
+			final int size = data1.length;
+			
+			double mean1 = mean(data1);
+			double mean2 = mean(data2);
+			double cov   = 0;
+			double var1  = 0;
+			double var2  = 0;
+			double diff1[] = new double[size];
+			double diff2[] = new double[size];
+			for(int i = 0; i < size; i++) {
+				double d1 = data1[i] - mean1;
+				double d2 = data2[i] - mean2;
+				cov  += d1 * d2;
+				var1 += d1 * d1;
+				var2 += d2 * d2;
+				diff1[i] = d1;
+				diff2[i] = d2;
+			}
+			correlation = cov / (Math.sqrt(var1) * Math.sqrt(var2));
+			covariance  = cov /size;
+			stats1      = new UniStats(size, mean1, var1 / size, diff1);
+			stats2      = new UniStats(size, mean2, var2 / size, diff2);
 		}
 		
 		public BiStats(UniStats stats1, UniStats stats2) {

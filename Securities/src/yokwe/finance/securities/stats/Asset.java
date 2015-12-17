7403c2e5188b183c497e3a0c0b0a264fff6f3dcb
yokwe <yokew@github.com>
2015-12-17 16:02:42 +0900

    Replace constructor with getInstance static method.

--- Securities/src/yokwe/finance/securities/stats/Asset.java
+++ Securities/src/yokwe/finance/securities/stats/Asset.java
@@ -28,7 +28,16 @@ public final class Asset {
 		this.price     = price;
 	}
 	
-	public Asset(Connection connection, String symbol, LocalDate dateFrom, LocalDate dateTo) {
+	@Override
+	public String toString() {
+		return String.format("{%-5s %5.2f %5.3f %5.3f}", symbol, lastPrice, dividend, divYield);
+	}
+	
+	public UniStats toUniStats() {
+		return new UniStats(DoubleArray.logReturn(price));
+	}
+	
+	public static Asset getInstance(Connection connection, String symbol, LocalDate dateFrom, LocalDate dateTo) {
 		List<PriceTable>    priceList    = PriceTable.getAllBySymbolDateRange(connection, symbol, dateFrom, dateTo);
 		List<DividendTable> dividendList = DividendTable.getAllBySymbolDateRange(connection, symbol, dateFrom, dateTo);
 		
@@ -45,20 +54,6 @@ public final class Asset {
 		double dividend = dividendList.stream().mapToDouble(o -> o.dividend).sum();
 		double price[] = priceList.stream().mapToDouble(o -> o.close).toArray();
 		
-		//
-		this.symbol    = symbol;
-		this.lastPrice = lastPrice;
-		this.dividend  = dividend;
-		this.divYield  = dividend / lastPrice;
-		this.price     = price;
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("{%-5s %5.2f %5.3f %5.3f}", symbol, lastPrice, dividend, divYield);
-	}
-	
-	public UniStats toUniStats() {
-		return new UniStats(DoubleArray.logReturn(price));
+		return new Asset(symbol, lastPrice, dividend, price);
 	}
 }

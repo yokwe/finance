ea70ae30a7461f035281eace35a61b50ccc2fb10
yokwe <yokew@github.com>
2015-12-17 16:01:16 +0900

    Add getBeta method.

--- Securities/src/yokwe/finance/securities/stats/FinStats.java
+++ Securities/src/yokwe/finance/securities/stats/FinStats.java
@@ -1,9 +1,11 @@
 package yokwe.finance.securities.stats;
 
-public class FinStats {
-	final double alpha;
-	final double beta;
-	final double r2;
+public final class FinStats {
+	public static final double MIN_R2_FOR_BETA = 0.7;
+	
+	public final double alpha;
+	public final double beta;
+	public final double r2;
 	
 	public FinStats(UniStats market, UniStats stock) {
 		this(market, stock, 0.0);
@@ -15,6 +17,10 @@ public class FinStats {
 		beta  = biStats.covariance / market.variance;
 		alpha = stock.mean - (interestRatesOfSafeAssets + beta * (market.mean - interestRatesOfSafeAssets));
 		r2    = biStats.correlation * biStats.correlation;
+	}
+	
+	public double getBeta() {
+		return (0.8 <= r2) ? beta : 0;
 	}
 	
 	@Override

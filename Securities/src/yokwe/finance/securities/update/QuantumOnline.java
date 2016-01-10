e0aaded68e812aa72f5c033f63d95ed3bf96b7c1
hasegawa <hasegawa.yasuhiro@gmail.com>
2016-01-10 20:03:53 +0900

    Add QuantumOnline (not finished).

---
+++ Securities/src/yokwe/finance/securities/update/QuantumOnline.java
@@ -0,0 +1,14 @@
+package yokwe.finance.securities.update;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Take security data from QuantamOnline.com
+//   http://www.quantumonline.com/search.cfm?tickersymbol=NRF-A&sopt=symbol
+
+public class QuantumOnline {
+	private static final Logger logger = LoggerFactory.getLogger(QuantumOnline.class);
+
+	public static void main(String[] args) {
+	}
+}

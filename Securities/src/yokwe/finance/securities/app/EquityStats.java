0a6b6db8cf85b8c5e6bb16147580225704725bb3
yokwe <yokew@github.com>
2016-02-15 16:49:35 +0900

    Add min and max.

--- Securities/src/yokwe/finance/securities/app/EquityStats.java
+++ Securities/src/yokwe/finance/securities/app/EquityStats.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -77,7 +78,7 @@ public class EquityStats {
 		}
 
 
-		w.write("symbol,name,price,sd,div,freq,changepct,count,hv,change,var95,var99,rsi,price200,divYield,beta,r2,vol5\n");
+		w.write("symbol,name,price,sd,div,freq,changepct,count,hv,change,var95,var99,rsi,price200,divYield,beta,r2,vol5,min,max\n");
 		for(String symbol: candidateList) {
 			List<PriceTable> priceTable = PriceTable.getAllBySymbolDateRange(connection, symbol, dateFrom, dateTo);
 			double priceArray[]  = priceTable.stream().mapToDouble(o -> o.close).toArray();
@@ -102,6 +103,7 @@ public class EquityStats {
 			Arrays.stream(priceArray).forEach(price200);
 			MA vol5 = MA.sma(5);
 			Arrays.stream(volumeArray).forEach(vol5);
+			DoubleSummaryStatistics summaryStats = Arrays.stream(priceArray).summaryStatistics();
 			
 			String name = nasdaqMap.get(symbol).name;
 			if (name.contains("\"")) name = name.replace("\"", "\"\"");
@@ -130,9 +132,9 @@ public class EquityStats {
 			}
 			
 			w.write(
-				String.format("%s,%s,%.2f,%.5f,%.2f,%d,%.4f,%d,%5f,%.5f,%.5f,%.5f,%.1f,%.3f,%.3f,%.3f,%.3f,%d\n",
+				String.format("%s,%s,%.2f,%.5f,%.2f,%d,%.4f,%d,%5f,%.5f,%.5f,%.5f,%.1f,%.3f,%.3f,%.3f,%.3f,%d,%.2f,%.2f\n",
 					symbol, name, price, stock.sd, div, freq, changepct, count, hv.getValue(), change, hv.getVaR95(1), hv.getVaR99(1),
-					rsi.getValue(), price200.getValue(), divYield, beta, r2, Math.round(vol5.getValue())));
+					rsi.getValue(), price200.getValue(), divYield, beta, r2, Math.round(vol5.getValue()), summaryStats.getMin(), summaryStats.getMax()));
 		}
 	}
 	

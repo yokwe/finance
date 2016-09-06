1439c054f6de03787f1840fea57e3087812e19f6
yokwe <hasegawa.yasuhiro+github.com>
2016-09-06 20:27:08 +0900

    Make field public.

--- Securities/src/yokwe/finance/securities/book/Securities.java
+++ Securities/src/yokwe/finance/securities/book/Securities.java
@@ -9,46 +9,46 @@ import yokwe.finance.securities.SecuritiesException;
 public class Securities {
 	static final org.slf4j.Logger logger = LoggerFactory.getLogger(Securities.class);
 
-	String symbol;
-	String tradeDate;
-	double quantities;
-	int    value; // value in JPY for tax declaration
+	public String symbol;
+	public String tradeDate;
+	public double quantity;
+	public int    value; // value in JPY for tax declaration
 	
-	private Securities(String symbol, String tradeDate, double quantities, int value) {
-		this.symbol     = symbol;
-		this.tradeDate  = tradeDate;
-		this.quantities = quantities;
-		this.value      = value;
+	private Securities(String symbol, String tradeDate, double quantity, int value) {
+		this.symbol    = symbol;
+		this.tradeDate = tradeDate;
+		this.quantity  = quantity;
+		this.value     = value;
 	}
 	
 	public static class Map {
 		private java.util.Map<String, Securities> map = new TreeMap<>();
 		
-		void buy(String symbol, double quantities, String tradeDate, int value) {
+		void buy(String symbol, double quantity, String tradeDate, int value) {
 			if (map.containsKey(symbol)) {
 				Securities securities = map.get(symbol);
 				securities.tradeDate  = tradeDate;
-				securities.quantities += quantities;
-				securities.value      += value;
+				securities.quantity  += quantity;
+				securities.value     += value;
 			} else {
-				map.put(symbol, new Securities(symbol, tradeDate, quantities, value));
+				map.put(symbol, new Securities(symbol, tradeDate, quantity, value));
 			}
 		}
 		
-		void sell(String symbol, double sellQuantities, String tradeDate, int value) {
+		void sell(String symbol, double sellQuantity, String tradeDate, int value) {
 			if (map.containsKey(symbol)) {
 				Securities securities = map.get(symbol);
 				
-				// See below for calculation of obtaining cost of sesurities.
+				// See below for calculation of obtaining cost of securities.
 				//   https://www.nta.go.jp/taxanswer/shotoku/1466.htm
-				double unitCost = Math.ceil(securities.value / securities.quantities);
-				double sellValue = Math.round(sellQuantities * unitCost);
+				double unitCost = Math.ceil(securities.value / securities.quantity);
+				double sellValue = Math.round(sellQuantity * unitCost);
 				
-				securities.tradeDate  = tradeDate;
-				securities.quantities = securities.quantities - sellQuantities;
-				securities.value      = (int)Math.round(securities.quantities * unitCost);
+				securities.tradeDate = tradeDate;
+				securities.quantity  = securities.quantity - sellQuantity;
+				securities.value     = (int)Math.round(securities.quantity * unitCost);
 				
-				logger.info("{}", String.format("%s %-8s  %10.5f  %8.0f  %8.0f", tradeDate, symbol, sellQuantities, unitCost, sellValue));
+				logger.info("{}", String.format("%s %-8s  %10.5f  %8.0f  %8.0f", tradeDate, symbol, sellQuantity, unitCost, sellValue));
 			} else {
 				logger.error("Unknown symbol = {}", symbol);
 				throw new SecuritiesException("Unexptected");

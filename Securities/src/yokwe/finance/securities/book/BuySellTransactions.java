99ad11ae4219c2fe54b9d876c891651de8f2122d
yokwe <hasegawa.yasuhiro+github.com>
2016-09-06 20:28:42 +0900

    Make field public. Add test code for NTA case.

--- Securities/src/yokwe/finance/securities/book/BuySellTransactions.java
+++ Securities/src/yokwe/finance/securities/book/BuySellTransactions.java
@@ -1,34 +1,90 @@
 package yokwe.finance.securities.book;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import yokwe.finance.securities.SecuritiesException;
+import yokwe.finance.securities.book.Securities.Map;
+
 @SheetData.SheetName("Buy-Sell-Transactions")
 public class BuySellTransactions extends SheetData {	
 	@ColumnName("YYYY-MM")
-	String yyyyMM;
+	public String yyyyMM;
 	@ColumnName("Page")
-	String page;
+	public String page;
 	@ColumnName("Transaction")
-	String transaction;
+	public String transaction;
 	@ColumnName("Date")
-	String date;
+	public String date;
 	@ColumnName("TradeDate")
-	String tradeDate;
+	public String tradeDate;
 	@ColumnName("Symbol")
-	String symbol;
+	public String symbol;
 	@ColumnName("Name")
-	String name;
+	public String name;
 	@ColumnName("Quantity")
-	double quantity;
+	public double quantity;
 	@ColumnName("Price")
-	double price;
+	public double price;
 	@ColumnName("Commission")
-	double commission;
+	public double commission;
 	@ColumnName("Debit")
-	double debit;
+	public double debit;
 	@ColumnName("Credit")
-	double credit;
+	public double credit;
 	
 	public String toString() {
 		return String.format("%s %s %-8s %s %s %-8s %-60s %7.2f %8.4f %5.2f %8.2f %8.2f",
 				yyyyMM, page, transaction, date, tradeDate, symbol, name, quantity, price, commission, debit, credit);
 	}
+	
+	static BuySellTransactions getTestInstance(String transaction, String tradeDate, String symbol, double quantity, double price) {
+		BuySellTransactions ret = new BuySellTransactions();
+		ret.yyyyMM      = "2000-01";
+		ret.page        = "1";
+		ret.transaction = transaction;
+		ret.tradeDate   = tradeDate;
+		ret.symbol      = symbol;
+		ret.quantity    = quantity;
+		ret.price       = price;
+		ret.debit       = transaction.equals("BOUGHT") ? quantity * price : 0;
+		ret.credit      = transaction.equals("SOLD") ? quantity * price : 0;
+		return ret;
+	}
+	static List<BuySellTransactions> getTestData() {
+		List<BuySellTransactions> ret = new ArrayList<>();
+		ret.add(getTestInstance("BOUGHT", "2001-05-01", "AAA", 5000, 800));
+		ret.add(getTestInstance("BOUGHT", "2001-08-01", "AAA", 2000, 850));
+		ret.add(getTestInstance("SOLD",   "2001-09-01", "AAA", 3000, 900));
+		ret.add(getTestInstance("BOUGHT", "2002-03-01", "AAA", 5000, 870));
+		ret.add(getTestInstance("SOLD",   "2001-09-01", "AAA", 6000, 950));
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		logger.info("START");
+		Map securitiesMap = new Map();
+		
+		List<BuySellTransactions> transactionList = getTestData();
+		for(BuySellTransactions transaction: transactionList) {
+			switch (transaction.transaction) {
+			case "BOUGHT": {
+				securitiesMap.buy(transaction.symbol, transaction.quantity, transaction.tradeDate, (int)Math.round(transaction.debit));
+				break;
+			}
+			case "SOLD": {
+				securitiesMap.sell(transaction.symbol, transaction.quantity, transaction.tradeDate, (int)Math.round(transaction.credit));
+				break;
+			}
+			default: {
+				logger.error("Unknown transaction = {}", transaction.transaction);
+				throw new SecuritiesException("Unexpected");
+			}
+			}
+		}
+		logger.info("STOP");
+		System.exit(0);
+	}
+
+	
 }

0110b996ba0bf7c7256b803fb5340a7e5dbf0416
yokwe <hasegawa.yasuhiro+github.com>
2016-09-24 12:04:32 +0900

    Now elapsed time of loading data is fast enough to use all fields.

--- Securities/src/yokwe/finance/securities/book/EquityStats.java
+++ Securities/src/yokwe/finance/securities/book/EquityStats.java
@@ -41,48 +41,48 @@ public class EquityStats extends SheetData {
 	
 	@ColumnName("symbol")
 	public String symbol;
-//	@ColumnName("name")
-//	public String name;
+	@ColumnName("name")
+	public String name;
 	@ColumnName("price")
 	public double price;
-//	@ColumnName("sd")
-//	public double sd;
-//	@ColumnName("div")
-//	public double div;
-//	@ColumnName("freq")
-//	public double freq;
-//	@ColumnName("changepct")
-//	public double changepct;
-//	@ColumnName("count")
-//	public int ount;
-//	@ColumnName("hv")
-//	public double hv;
-//	@ColumnName("change")
-//	public double change;
-//	@ColumnName("var95")
-//	public double var95;
-//	@ColumnName("var99")
-//	public double var99;
-//	@ColumnName("rsi")
-//	public double rsi;
-//	@ColumnName("price200")
-//	public double price200;
-//	@ColumnName("divYield")
-//	public double divYield;
-//	@ColumnName("beta")
-//	public double beta;
-//	@ColumnName("r2")
-//	public double r2;
-//	@ColumnName("vol5")
-//	public int vol5;
-//	@ColumnName("min")
-//	public double min;
-//	@ColumnName("max")
-//	public double max;
-//	@ColumnName("minpct")
-//	public double minpct;
-//	@ColumnName("maxpct")
-//	public double maxpct;
+	@ColumnName("sd")
+	public double sd;
+	@ColumnName("div")
+	public double div;
+	@ColumnName("freq")
+	public double freq;
+	@ColumnName("changepct")
+	public double changepct;
+	@ColumnName("count")
+	public int ount;
+	@ColumnName("hv")
+	public double hv;
+	@ColumnName("change")
+	public double change;
+	@ColumnName("var95")
+	public double var95;
+	@ColumnName("var99")
+	public double var99;
+	@ColumnName("rsi")
+	public double rsi;
+	@ColumnName("price200")
+	public double price200;
+	@ColumnName("divYield")
+	public double divYield;
+	@ColumnName("beta")
+	public double beta;
+	@ColumnName("r2")
+	public double r2;
+	@ColumnName("vol5")
+	public int vol5;
+	@ColumnName("min")
+	public double min;
+	@ColumnName("max")
+	public double max;
+	@ColumnName("minpct")
+	public double minpct;
+	@ColumnName("maxpct")
+	public double maxpct;
 
 	
 	public static void main(String[] args) {

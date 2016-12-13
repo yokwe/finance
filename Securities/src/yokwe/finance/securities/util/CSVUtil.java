2e28a9de0ab8170909796534d12ab169877eb49a
yokwe <hasegawa.yasuhiro+github@gmail.com>
2016-12-13 16:31:46 +0900

    Add more sanity check code.

--- Securities/src/yokwe/finance/securities/util/CSVUtil.java
+++ Securities/src/yokwe/finance/securities/util/CSVUtil.java
@@ -46,6 +46,7 @@ public class CSVUtil {
 		try (CSVParser csvParser = csvFormat.parse(new FileReader(file))) {
 			for(CSVRecord record: csvParser) {
 				if (record.size() != size) {
+					// Sanity check
 					logger.error("record.size != size  {} != {}", record.size(), size);
 					throw new SecuritiesException("record.size != size");
 				}

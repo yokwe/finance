6834b73ceac6493e56ea747b2db5ba524721a72f
yokwe <hasegawa.yasuhiro+github@gmail.com>
2016-12-15 16:47:13 +0900

    Reorganize getURL and getPath.

--- Securities/src/yokwe/finance/securities/quandl/Quandl.java
+++ Securities/src/yokwe/finance/securities/quandl/Quandl.java
@@ -6,18 +6,23 @@ import yokwe.finance.securities.util.FileUtil;
 
 public final class Quandl {
 	public static final String URL_BASE = "https://www.quandl.com/api/v3";
+	public static final String PATH_BASE = "tmp/fetch/quandl";
 	
 	private static final String PATH_API_KEY = "tmp/fetch/quandl/api_key";
 	private static final String API_KEY;
 	static {
-		API_KEY = FileUtil.getContents(new File(PATH_API_KEY));
-	}
-		
-	public static String getURL(String name, String format, int page) {
-		return String.format("%s/%s.%s?page=%d&api_key=%s", URL_BASE, name, format, page, API_KEY);
+		API_KEY = FileUtil.read(new File(PATH_API_KEY));
 	}
 	
-	public static String getURL(String database_code) {
-		return String.format("%s/databases/%s/codes?api_key=%s", URL_BASE, database_code, API_KEY);
+	public static String getURL(String path) {
+		return String.format("%s/%s?api_key=%s", URL_BASE, path, API_KEY);
+	}
+	
+	public static String getURL(String path, String query) {
+		return String.format("%s/%s?api_key=%s&%s", URL_BASE, path, API_KEY, query);
+	}
+	
+	public static String getPath(String path) {
+		return String.format("%s/%s", PATH_BASE, path);
 	}
 }

7df91d0d1badfc443eb9f699b3d0b125ee290cd6
yokwe <hasegawa.yasuhiro+github.com>
2016-12-16 23:23:38 +0900

    Use another way to get metadat of datasets.

--- Securities/src/yokwe/finance/securities/quandl/DatasetList.java
+++ Securities/src/yokwe/finance/securities/quandl/DatasetList.java
@@ -1,155 +1,155 @@
 package yokwe.finance.securities.quandl;
 
-import java.io.ByteArrayInputStream;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
+
 import yokwe.finance.securities.SecuritiesException;
-import yokwe.finance.securities.util.CSVUtil;
 import yokwe.finance.securities.util.HttpUtil;
 
 public class DatasetList {
 	private static final Logger logger = LoggerFactory.getLogger(DatasetList.class);
+	
+	public static final String PATH_DIR = "metadata/datasets";
+	
+	private static final int BUFFER_SIZE = 64 * 1024;
+	
+	private static final CSVFormat csvFormat = CSVFormat.DEFAULT.withRecordSeparator("\n");
 
-	public static class Entry {
-		public String key;
-		public String description;
-	}
-	
-	public static final String PATH_DIR = Quandl.getPath("metadata/datasets/codes");
-	
-	public static String getURL(String database_code) {
-		String path = String.format("databases/%s/codes", database_code);
-		return Quandl.getURL(path);
-	}
 	
 	public static String getPath(String database_code) {
-		return String.format("%s/%s.csv", PATH_DIR, database_code);
+		String path = String.format("%s/%s.csv", PATH_DIR, database_code);
+		
+		return Quandl.getPath(path);
+	}
+	
+	//https://www.quandl.com/api/v3/datasets.json?database_code=ODA&page=1
+	public static String getURL(String database_code, int page) {
+		String path  = "datasets.json";
+		String query = String.format("database_code=%s&page=%d", database_code, page);
+		
+		return Quandl.getURL(path, query);
 	}
 
-	public static void save(String database_code) {
-		String url = getURL(database_code);
-//		logger.info("url = {}", url);
-		byte[] zipData  = HttpUtil.downloadAsByteArray(url);
-		if (zipData == null) {
-//			logger.warn("Failed to load {}", database_code);
-			return;
-		}
-//		logger.info("zpiData {}", zipData.length);
-		
-		File dir = new File(PATH_DIR);
-		dir.mkdirs();
-		
-		try (ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(zipData))) {
-			for(;;) {
-				ZipEntry zipEntry = zis.getNextEntry();
-				if (zipEntry == null) break;
-//				logger.info("zipEntry {} {}", zipEntry.getName(), zipEntry.getSize());
+	public static String toString(String[] strings) {
+		StringBuilder ret = new StringBuilder();
+		for(int i = 0; i < strings.length; i++) {
+			String string = strings[i];
+			if (string.matches("[\\\",]")) {
 				
-				// save content to file
-				String outPath = getPath(database_code);
-//				logger.info("outPath {}", outPath);
-				File outFile = new File(outPath);
-				try (FileOutputStream fos = new FileOutputStream(outFile)) {
-					byte[] buf = new byte[64 * 1024];
-					for(;;) {
-						int len = zis.read(buf, 0, buf.length);
-						if (len == -1) break;
-						fos.write(buf, 0, len);
-					}
-				}
-				
-				zis.closeEntry();
 			}
-		} catch (IOException e) {
-			logger.error("IOException {}", e.toString());
-			throw new SecuritiesException("IOException");
-		}
-	}
-	
-	public static void saveAll() {
-		List<DatabaseList.Entry> databaseList = DatabaseList.getAll();
-		int i = 1;
-		for(DatabaseList.Entry database: databaseList) {
-			logger.info("database {} / {}  {}", i++, databaseList.size(), database.database_code);
-			DatasetList.save(database.database_code);
-		}
-	}
-	
-	public static List<Entry> load(String database_code) {
-		String path = getPath(database_code);
-		List<Entry> entries = CSVUtil.load(path, Entry.class);
-		return entries;
-	}
-	
-	public static List<Entry> loadAll() {
-		List<DatabaseList.Entry> databaseList = DatabaseList.getAll();
-		logger.info("databaseList {}", databaseList.size());
-		
-		List<Entry> ret = new ArrayList<>();
-		int i = 1;
-		for(DatabaseList.Entry database: databaseList) {
-			logger.info("database {} / {}  {}", i++, databaseList.size(), database.database_code);
-			List<Entry> entries = load(database.database_code);
-			ret.addAll(entries);
+			if (0 < i) ret.append(",");
+			
+			ret.append(strings[i]);
 		}
 		
-		logger.info("ret {}", ret.size());
-		return ret;
+		return ret.toString();
 	}
 	
+	public static class Entry {
+		public int     id;
+		public String  dataset_code;
+		public String  database_code;
+		public String  name;
+		public String  description;
+		public String  refreshed_at;
+		public String  newest_available_date;
+		public String  oldest_available_date;
+		public String  column_names[];
+		public String  frequency; // annual
+		public String  type;      // Time Series
+		public boolean premium;
+		public int     database_id;
+	}
+	
+	
+	public List<Entry> datasets;
+	public Quandl.Meta meta;
+	
+	private static void save(CSVPrinter csvPrinter, DatasetList datasets) throws IOException {
+		for(Entry entry: datasets.datasets) {
+			csvPrinter.printRecord(
+					entry.id,
+					entry.dataset_code,
+					entry.database_code,
+					entry.name,
+					entry.description,
+					entry.refreshed_at,
+					entry.newest_available_date,
+					entry.oldest_available_date,
+					csvFormat.format((Object[])entry.column_names),
+					entry.frequency,
+					entry.type,
+					entry.premium,
+					entry.database_id
+					);
+		}
+	}
+	
+	public static void save(CSVPrinter csvPrinter, String database_code) throws IOException {
+		Gson gson = new Gson();
+
+		int totalPages;
+		int totalCount;
+
+		{
+			String url = getURL(database_code, 1);
+//			logger.info("url = {}", url);
+			String json = HttpUtil.downloadAsString(url);
+//			logger.info("json = {}", json);
+			DatasetList datasets = gson.fromJson(json, DatasetList.class);
+			totalPages = datasets.meta.total_pages;
+			totalCount = datasets.meta.total_count;
+//			logger.info("totalPages = {}", totalPages);
+			logger.info("totalCount = {}", totalCount);
+			logger.info("{} / {}", 1, totalPages);
+			save(csvPrinter, datasets);
+		}
+		
+		for(int i = 2; i <= totalPages; i++) {
+			String url = getURL(database_code, i);
+//			logger.info("url = {}", url);
+			String json = HttpUtil.downloadAsString(url);
+//			logger.info("json = {}", json);
+			DatasetList datasets = gson.fromJson(json, DatasetList.class);
+			logger.info("{} / {}", i, totalPages);
+			save(csvPrinter, datasets);
+		}
+		
+	}
+
 	public static void update() {
 		logger.info("START");
 		List<DatabaseList.Entry> databaseList = DatabaseList.load();
 		
+		// Create parent directory if it doesn't exist.
 		{
-			int size = databaseList.size();
-			int i = 0;
-			for(DatabaseList.Entry entry: databaseList) {
-				i++;
-				String database_code = entry.database_code;
-				String path = getPath(database_code);
-				File file = new File(path);
-				if (file.exists()) {
-					logger.info("{}  check {}", String.format("%4d / %4d", i, size), database_code);
-					// Sanity check
-					// Try load csv file and make sure it can readable
-					load(database_code);
-				} else {
-					logger.info("{}  save  {}", String.format("%4d / %4d", i, size), database_code);
-					save(database_code);
-					// save can failed. So don't do sanity check here.
-				}
-			}
+			File dir = new File(PATH_DIR);
+			if (!dir.exists()) dir.mkdirs();
 		}
 		
-		// find removed entry from databaseList
-		{
-			Set<String> set = new TreeSet<>();
-			for(DatabaseList.Entry entry: databaseList) {
-				set.add(entry.database_code);
-			}
+		for(DatabaseList.Entry database: databaseList) {
+			String database_code = database.database_code;
+			String path = getPath(database_code);
+			File   file = new File(path);
+			if (file.exists()) continue;
 			
-			File[] files = new File(PATH_DIR).listFiles();
-			for(File file: files) {
-				String fileName = file.getName();
-				if (fileName.endsWith(".csv")) {
-					String database_code = fileName.substring(0, fileName.length() - 4);
-					if (!set.contains(database_code)) {
-						logger.info("This file is not in databaseList. remove {}", fileName);
-						file.delete();
-					}
-				}
+			logger.info("database_code {}", database_code);
+
+			try (CSVPrinter csvPrint = new CSVPrinter(new BufferedWriter(new FileWriter(path), BUFFER_SIZE), csvFormat)) {
+				save(csvPrint, database_code);
+			} catch (IOException e) {
+				logger.error("IOException {}", e.toString());
+				throw new SecuritiesException("IOException");
 			}
 		}
 		logger.info("STOP");

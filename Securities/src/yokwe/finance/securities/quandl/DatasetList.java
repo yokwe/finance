0033c80765cd8ee9bc6708dd145fca8f101b3041
yokwe <hasegawa.yasuhiro+github.com>
2016-12-17 20:12:18 +0900

    Merge with existing csv file.

--- Securities/src/yokwe/finance/securities/quandl/DatasetList.java
+++ Securities/src/yokwe/finance/securities/quandl/DatasetList.java
@@ -1,13 +1,20 @@
 package yokwe.finance.securities.quandl;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -73,31 +80,37 @@ public class DatasetList {
 		public int     database_id;
 	}
 	
-	
 	public List<Entry> datasets;
-	public Quandl.Meta meta;
+	public Quandl.Meta meta;	
 	
-	private static void save(CSVPrinter csvPrinter, DatasetList datasets) throws IOException {
+	private static void update(CSVPrinter csvPrinter, DatasetList datasets, Set<Integer> idSet) throws IOException {
 		for(Entry entry: datasets.datasets) {
-			csvPrinter.printRecord(
-					entry.id,
-					entry.dataset_code,
-					entry.database_code,
-					entry.name,
-					entry.description,
-					entry.refreshed_at,
-					entry.newest_available_date,
-					entry.oldest_available_date,
-					csvFormat.format((Object[])entry.column_names),
-					entry.frequency,
-					entry.type,
-					entry.premium,
-					entry.database_id
-					);
+			Integer id = entry.id;
+			if (idSet.contains(id)) {
+//				logger.warn("dup id {}", id);
+			} else {
+//				logger.info("add id {}", id);
+				csvPrinter.printRecord(
+						entry.id,
+						entry.dataset_code,
+						entry.database_code,
+						entry.name,
+						entry.description,
+						entry.refreshed_at,
+						entry.newest_available_date,
+						entry.oldest_available_date,
+						csvFormat.format((Object[])entry.column_names),
+						entry.frequency,
+						entry.type,
+						entry.premium,
+						entry.database_id
+						);
+				idSet.add(id);
+			}
 		}
 	}
 	
-	public static void save(CSVPrinter csvPrinter, String database_code) throws IOException {
+	private static void update(CSVPrinter csvPrinter, String database_code, Set<Integer> idSet) throws IOException {
 		Gson gson = new Gson();
 
 		int totalPages;
@@ -112,9 +125,12 @@ public class DatasetList {
 			totalPages = datasets.meta.total_pages;
 			totalCount = datasets.meta.total_count;
 //			logger.info("totalPages = {}", totalPages);
-			logger.info("totalCount = {}", totalCount);
+			logger.info("{} totalCount = {}", database_code, totalCount);
+			
+			if (totalCount == idSet.size()) return;
+			
 			logger.info("{}  {} / {}", database_code, 1, totalPages);
-			save(csvPrinter, datasets);
+			update(csvPrinter, datasets, idSet);
 		}
 		
 		for(int i = 2; i <= totalPages; i++) {
@@ -124,7 +140,53 @@ public class DatasetList {
 //			logger.info("json = {}", json);
 			DatasetList datasets = gson.fromJson(json, DatasetList.class);
 			logger.info("{}  {} / {}", database_code, i, totalPages);
-			save(csvPrinter, datasets);
+			update(csvPrinter, datasets, idSet);
+		}
+		
+		logger.info("{} new {} / {}", database_code, idSet.size(), totalCount);
+	}
+	
+	private static void update(String database_code) {
+		String path = getPath(database_code);
+		File   file = new File(path);
+		
+		Set<Integer> idSet = new TreeSet<>();
+		if (file.exists()) {
+			try (CSVParser csvParser = csvFormat.parse(new BufferedReader(new FileReader(path), BUFFER_SIZE))) {
+				for(CSVRecord record: csvParser) {
+					String value = record.get(0);
+					Integer id = Integer.valueOf(value);
+					
+					if (idSet.contains(id)) {
+						// duplicate
+						logger.warn("dup in existing  {}", id);
+					} else {
+						// new
+						idSet.add(id);
+					}
+				}
+//				logger.info("{} id {} existing", database_code, idSet.size());
+				logger.info("{} existing   = {}", database_code, idSet.size());
+			} catch (FileNotFoundException e) {
+				logger.error("FileNotFoundException {}", e.toString());
+				throw new SecuritiesException("FileNotFoundException");
+			} catch (IOException e) {
+				logger.error("IOException {}", e.toString());
+				throw new SecuritiesException("IOException");
+			}
+		}
+		
+		boolean deleteFile = true;
+		try (CSVPrinter csvPrinter = new CSVPrinter(new BufferedWriter(new FileWriter(path, true), BUFFER_SIZE), csvFormat)) {
+			update(csvPrinter, database_code, idSet);
+			// processing is completed.
+			deleteFile = false;
+		} catch (IOException e) {
+			logger.error("IOException {}", e.toString());
+			throw new SecuritiesException("IOException");
+		} finally {
+			// delete file when processing is not completed.
+			if (deleteFile) file.delete();
 		}
 	}
 
@@ -140,24 +202,8 @@ public class DatasetList {
 		
 		for(DatabaseList.Entry database: databaseList) {
 			String database_code = database.database_code;
-			String path = getPath(database_code);
-			File   file = new File(path);
-			if (file.exists()) continue;
-			
-			logger.info("database_code {}", database_code);
-
-			boolean deleteFile = true;
-			try (CSVPrinter csvPrint = new CSVPrinter(new BufferedWriter(new FileWriter(path), BUFFER_SIZE), csvFormat)) {
-				save(csvPrint, database_code);
-				// processing is completed.
-				deleteFile = false;
-			} catch (IOException e) {
-				logger.error("IOException {}", e.toString());
-				throw new SecuritiesException("IOException");
-			} finally {
-				// delete file when processing is not completed.
-				if (deleteFile) file.delete();
-			}
+//			if (!database_code.equals("SYSPEACE")) continue;
+			update(database_code);
 		}
 		logger.info("STOP");
 	}

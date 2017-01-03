38f614322bdd414ff8237177e360a95f37d06d7c
yokwe <hasegawa.yasuhiro+github.com>
2017-01-03 19:34:18 +0900

    Remove unused import.

--- Securities/src/yokwe/finance/securities/util/CSVUtil.java
+++ Securities/src/yokwe/finance/securities/util/CSVUtil.java
@@ -2,7 +2,6 @@ package yokwe.finance.securities.util;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;

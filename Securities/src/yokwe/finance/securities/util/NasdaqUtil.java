7b2229ac179d70e29bb67ebfd537d9498a52ad18
yokwe <hasegawa.yasuhiro+github.com>
2017-01-18 23:06:04 +0900

    Use CSVUtil to load data from file.

--- Securities/src/yokwe/finance/securities/util/NasdaqUtil.java
+++ Securities/src/yokwe/finance/securities/util/NasdaqUtil.java
@@ -1,10 +1,7 @@
 package yokwe.finance.securities.util;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -22,32 +19,9 @@ public class NasdaqUtil {
 	private static Map<String, NasdaqTable> map = new TreeMap<>();
 	
 	static {
-		char[] buffer = new char[65536];
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(new File(NASDAQ_FILE_PATH)), buffer.length)) {
-			for(;;) {
-				String line = br.readLine();
-				if (line == null) break;
-				
-				String[] fields = line.split(",", 8);
-				String etf    = fields[0];
-				String exch   = fields[1];
-				String symbol = fields[2];
-				String base   = fields[3];
-				String yahoo  = fields[4];
-				String google = fields[5];
-				String nasdaq = fields[6];
-				String name   = fields[7];
-				
-				if (name.charAt(0) == '"') name = name.substring(1, name.length() - 1);
-				if (name.contains("\"\"")) name = name.replace("\"\"", "\"");
-				
-				map.put(symbol, new NasdaqTable(etf, exch, symbol, base, yahoo, google, nasdaq, name));
-			}
-		} catch (IOException e) {
-			logger.error(e.getClass().getName());
-			logger.error(e.getMessage());
-			throw new SecuritiesException();
+		List<NasdaqTable> tableList = CSVUtil.load(NASDAQ_FILE_PATH, NasdaqTable.class);
+		for(NasdaqTable table: tableList) {
+			map.put(table.symbol, table);
 		}
 	}
 	

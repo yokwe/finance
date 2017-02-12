bdc73a2c3d32a59b05ee9f62ac5873df0f301792
yokwe <hasegawa.yasuhiro+github.com>
2017-02-12 13:14:54 +0900

    Use round to decimal point 2 to get buy value.

--- Securities/src/yokwe/finance/securities/tax/Transfer.java
+++ Securities/src/yokwe/finance/securities/tax/Transfer.java
@@ -32,7 +32,7 @@ public class Transfer {
 			this.price         = price;
 			this.fee           = fee;
 			this.fxRate        = fxRate;
-			this.buy           = this.price * this.quantity;
+			this.buy           = DoubleUtil.round(this.price * this.quantity, 2);
 			this.buyJPY        = (int)Math.floor(this.buy * this.fxRate);
 			this.feeJPY        = (int)Math.floor(this.fee * this.fxRate);
 			this.totalQuantity = totalQuantity;

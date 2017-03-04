d14736689e30ae55230f83806e210adb8fff02ab
yokwe <hasegawa.yasuhiro+github.com>
2017-03-04 21:12:02 +0900

    Add read method that take character encoding.

--- Securities/src/yokwe/finance/securities/util/FileUtil.java
+++ Securities/src/yokwe/finance/securities/util/FileUtil.java
@@ -3,9 +3,11 @@ package yokwe.finance.securities.util;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,23 +18,27 @@ public class FileUtil {
 	private static final Logger logger = LoggerFactory.getLogger(FileUtil.class);
 
 	public static String read(File file) {
-		char[] buffer = new char[65536];
-		
-		StringBuilder ret = new StringBuilder();
-		
-		try (BufferedReader bfr = new BufferedReader(new FileReader(file), buffer.length)) {
+		String encoding = Charset.defaultCharset().name();
+		return read(file, encoding);
+	}
+	
+	public static String read(File file, String encoding) {
+		char[] buffer = new char[65536];		
+		try (BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding), buffer.length)) {
+			StringBuilder ret = new StringBuilder();
+			
 			for(;;) {
 				int len = bfr.read(buffer);
 				if (len == -1) break;
 				
 				ret.append(buffer, 0, len);
 			}
+			return ret.toString();
 		} catch (IOException e) {
 			logger.error(e.getClass().getName());
 			logger.error(e.getMessage());
 			throw new SecuritiesException();
 		}
-		return ret.toString();
 	}
 	
 	public static void write(File file, String content) {

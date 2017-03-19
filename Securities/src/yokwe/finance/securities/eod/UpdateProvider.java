32385938adcc6c20cecc9c60111c8d660e0e81d8
yokwe <hasegawa.yasuhiro+github.com>
2017-03-19 21:15:27 +0900

    Take stock information from 2015-11-01.

--- Securities/src/yokwe/finance/securities/eod/UpdateProvider.java
+++ Securities/src/yokwe/finance/securities/eod/UpdateProvider.java
@@ -4,10 +4,15 @@ import java.io.File;
 import java.time.LocalDate;
 
 public interface UpdateProvider {
-	public static final String GOOGLE = "google";
-	public static final String YAHOO  = "yahoo";
+	public static final LocalDate DATE_FIRST    = LocalDate.of(2015, 11, 1);
+	public static final LocalDate DATE_LAST     = Market.getLastTradingDate();
+	
+	public static final int       MAX_RETRY     = 3;  // try 3 times at least
+	
+	public static final String    GOOGLE        = "google";
+	public static final String    YAHOO         = "yahoo";
 
 	public String  getName();
 	public File    getFile(String symbol);
-	public boolean updateFile(String exch, String symbol, LocalDate dateFirst, LocalDate dateLast);
+	public boolean updateFile(String exch, String symbol);
 }

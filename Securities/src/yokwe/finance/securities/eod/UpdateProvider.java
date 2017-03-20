b48f5167c3800b6254f89abb0e6c9a505b903224
yokwe <hasegawa.yasuhiro+github.com>
2017-03-21 08:11:04 +0900

    Change DATE_FIRST again to take one year data. Add parameter dateFirst and dateLast to updateFile again.

--- Securities/src/yokwe/finance/securities/eod/UpdateProvider.java
+++ Securities/src/yokwe/finance/securities/eod/UpdateProvider.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.time.LocalDate;
 
 public interface UpdateProvider {
-	public static final LocalDate DATE_FIRST    = LocalDate.of(2015, 1, 1);
 	public static final LocalDate DATE_LAST     = Market.getLastTradingDate();
+	public static final LocalDate DATE_FIRST    = DATE_LAST.minusYears(1);
 	
 	public static final int       MAX_RETRY     = 3;  // try 3 times at least
 	
@@ -14,5 +14,9 @@ public interface UpdateProvider {
 
 	public String  getName();
 	public File    getFile(String symbol);
-	public boolean updateFile(String exch, String symbol, boolean newFile);
+	public boolean updateFile(String exch, String symbol, boolean newFile, LocalDate dateFirst, LocalDate dateLast);
+	
+	public default boolean updateFile(String exch, String symbol, boolean newFile) {
+		return updateFile(exch, symbol, newFile, DATE_FIRST, DATE_FIRST);
+	}
 }

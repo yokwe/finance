d9a364407f0ec83e60c9c7faf1d6093c7d498979
yokwe <hasegawa.yasuhiro+github@gmail.com>
2017-03-21 16:51:39 +0900

    Add StockGain calss.

---
+++ Securities/src/yokwe/finance/securities/eod/report/StockGain.java
@@ -0,0 +1,74 @@
+package yokwe.finance.securities.eod.report;
+
+import yokwe.finance.securities.libreoffice.Sheet;
+import yokwe.finance.securities.libreoffice.SpreadSheet;
+
+@Sheet.SheetName("StockGain")
+@Sheet.HeaderRow(0)
+@Sheet.DataRow(1)
+public class StockGain extends Sheet implements Comparable<StockGain> {
+	@ColumnName("Date")
+	@NumberFormat(SpreadSheet.FORMAT_DATE)
+	public String date;       // YYYY-MM
+	
+	@ColumnName("Stock")
+	@NumberFormat(SpreadSheet.FORMAT_NUMBER2)
+	public double stock;
+
+	@ColumnName("Unreal")
+	@NumberFormat(SpreadSheet.FORMAT_NUMBER2)
+	public double unreal;
+
+	@ColumnName("Unreal Gain")
+	@NumberFormat(SpreadSheet.FORMAT_NUMBER2)
+	public double unrealGain;
+
+	@ColumnName("Buy")
+	@NumberFormat(SpreadSheet.FORMAT_NUMBER2_BLANK)
+	public double buy;
+	
+	@ColumnName("Sell")
+	@NumberFormat(SpreadSheet.FORMAT_NUMBER2_BLANK)
+	public double sell;
+	
+	@ColumnName("Sell Gain")
+	@NumberFormat(SpreadSheet.FORMAT_NUMBER2_BLANK)
+	public double sellGain;
+	
+	@ColumnName("Real Gain")
+	@NumberFormat(SpreadSheet.FORMAT_NUMBER2)
+	public double realGain;
+
+	public StockGain(String date, double stock, double unreal, double unrealGain, double buy, double sell, double sellGain, double realGain) {
+		this.date       = date;
+		this.stock      = stock;
+		this.unreal     = unreal;
+		this.unrealGain = unrealGain;
+		this.buy        = buy;
+		this.sell       = sell;
+		this.sellGain   = sellGain;
+		this.realGain   = realGain;
+	}
+	public StockGain() {
+		this("", 0, 0, 0, 0, 0, 0, 0);
+	}
+	public StockGain(StockGain that) {
+		this.date       = that.date;
+		this.stock      = that.stock;
+		this.unreal     = that.unreal;
+		this.unrealGain = that.unrealGain;
+		this.buy        = that.buy;
+		this.sell       = that.sell;
+		this.sellGain   = that.sellGain;
+		this.realGain   = that.realGain;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f", date, stock, unreal, unrealGain, buy, sell, sellGain, realGain);
+	}
+	@Override
+	public int compareTo(StockGain that) {
+		return this.date.compareTo(that.date);
+	}
+}

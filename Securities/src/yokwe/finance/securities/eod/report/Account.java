5f9dc3c967721542a4feb710816c4df5f65eb333
yokwe <hasegawa.yasuhiro+github@gmail.com>
2017-03-21 16:22:40 +0900

    Remove unrelGain.

--- Securities/src/yokwe/finance/securities/eod/report/Account.java
+++ Securities/src/yokwe/finance/securities/eod/report/Account.java
@@ -75,14 +75,10 @@ public class Account extends Sheet implements Comparable<Account> {
 	@NumberFormat(SpreadSheet.FORMAT_NUMBER2_BLANK)
 	public double sellGain;   // sell gain for this month
 	
-	@ColumnName("Unreal Gain")
-	@NumberFormat(SpreadSheet.FORMAT_NUMBER2_BLANK)
-	public double unrealGain;   // unrealized gain for this month
-		
-	
+
 	public Account(String date, double fundTotal, double cashTotal, double stockTotal, double gainTotal,
 			double wireIn, double wireOut, double achIn, double achOut, double interest, double dividend,
-			String symbol, double buy, double sell, double sellCost, double sellGain, double unrealGain) {
+			String symbol, double buy, double sell, double sellCost, double sellGain) {
 		this.date       = date;
 		this.fundTotal  = fundTotal;
 		this.cashTotal  = cashTotal;
@@ -101,22 +97,21 @@ public class Account extends Sheet implements Comparable<Account> {
 		this.sell       = sell;
 		this.sellCost   = sellCost;
 		this.sellGain   = sellGain;
-		this.unrealGain = unrealGain;
 	}
 	public Account() {
 		this("", 0, 0, 0, 0,
 				0, 0, 0, 0, 0, 0,
-				"", 0, 0, 0, 0, 0);
+				"", 0, 0, 0, 0);
 	}
 	public Account(Account that) {
 		this(new String(that.date), that.fundTotal, that.cashTotal, that.stockTotal, that.gainTotal,
 				that.wireIn, that.wireOut, that.achIn, that.achOut, that.interest, that.dividend,
-				that.symbol, that.buy, that.sell, that.sellCost, that.sellGain, that.unrealGain);
+				that.symbol, that.buy, that.sell, that.sellCost, that.sellGain);
 	}
 	@Override
 	public String toString() {
-		return String.format("%s %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %-10s %9.2f %9.2f %9.2f %9.2f %9.2f", 
-			date, fundTotal, cashTotal, stockTotal, gainTotal, wireIn, wireOut, achIn, achOut, interest, dividend, symbol, buy, sell, sellCost, sellGain, unrealGain);
+		return String.format("%s %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %9.2f %-10s %9.2f %9.2f %9.2f %9.2f", 
+			date, fundTotal, cashTotal, stockTotal, gainTotal, wireIn, wireOut, achIn, achOut, interest, dividend, symbol, buy, sell, sellCost, sellGain);
 	}
 	@Override
 	public int compareTo(Account that) {

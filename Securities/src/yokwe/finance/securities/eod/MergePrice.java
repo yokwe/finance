8140c4efafea0b47a346c2e37cade4d349eed800
yokwe <hasegawa.yasuhiro+github@gmail.com>
2017-03-21 11:36:03 +0900

    Add progress log message.

--- Securities/src/yokwe/finance/securities/eod/MergePrice.java
+++ Securities/src/yokwe/finance/securities/eod/MergePrice.java
@@ -10,81 +10,6 @@ import yokwe.finance.securities.SecuritiesException;
 public class MergePrice {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(MergePrice.class);
 
-	private static final UpdateProvider priceGoogleProvider   = UpdatePrice.getProvider(UpdateProvider.GOOGLE);
-	private static final UpdateProvider priceYahooProvider    = UpdatePrice.getProvider(UpdateProvider.YAHOO);
-	
-	private static int count       = 0;
-	private static int countGoogle = 0;
-	private static int countYahoo  = 0;
-
-	public static void mergeFile(String symbol) {
-		File file = Price.getFile(symbol);
-		
-		File priceGoogle   = priceGoogleProvider.getFile(symbol);
-		File priceYahoo    = priceYahooProvider.getFile(symbol);
-		
-		List<Price> priceList = null;
-		
-		if (priceGoogle.exists() && priceYahoo.exists()) {
-			// both
-			List<Price> priceListGoogle = Price.load(priceGoogle);
-			List<Price> priceListYahoo  = Price.load(priceYahoo);
-			
-			String dateGoogle = priceListGoogle.get(0).date;
-			String dateYahoo  = priceListYahoo.get(0).date;
-			
-			if (dateGoogle.equals(UpdateProvider.DATE_LAST) && dateYahoo.equals(UpdateProvider.DATE_LAST)) {
-				// both
-				int priceGoogleSize = priceListGoogle.size();
-				int priceYahooSize  = priceListYahoo.size();
-				
-				if (priceGoogleSize <= priceYahooSize) { // prefer yahoo over google
-					priceList = priceListYahoo;
-					countYahoo++;
-				} else {
-					priceList = priceListGoogle;
-					countGoogle++;
-				}
-			} else if (dateGoogle.equals(UpdateProvider.DATE_LAST)) {
-				// google
-				priceList = priceListGoogle;
-				countGoogle++;
-			} else if (dateYahoo.equals(UpdateProvider.DATE_LAST)) {
-				// yahoo
-				priceList = priceListYahoo;
-				countYahoo++;
-			} else {
-				// none -- could be happen for discontinued stock
-				int priceGoogleSize = priceListGoogle.size();
-				int priceYahooSize  = priceListYahoo.size();
-				
-				if (priceGoogleSize < priceYahooSize) {
-					priceList = priceListYahoo;
-					countYahoo++;
-				} else {
-					priceList = priceListGoogle;
-					countGoogle++;
-				}
-			}
-		} else if (priceGoogle.exists()) {
-			// only google
-			priceList = Price.load(priceGoogle);
-			countGoogle++;
-		} else if (priceYahoo.exists()) {
-			// only yahoo
-			priceList = Price.load(priceYahoo);
-			countYahoo++;
-		} else {
-			// none
-//			logger.warn("{}  skip   {}", String.format("%4d / %4d",  count, total), String.format("%-8s NO PRICE DATA", symbol));
-		}
-		
-		if (priceList != null) {
-			Price.save(priceList, file);
-			count++;
-		}
-	}
-	
 	public static void main(String[] args) {
 		logger.info("START");
 
@@ -106,12 +31,98 @@ public class MergePrice {
 			}
 		}
 
+		UpdateProvider priceGoogleProvider   = UpdatePrice.getProvider(UpdateProvider.GOOGLE);
+		UpdateProvider priceYahooProvider    = UpdatePrice.getProvider(UpdateProvider.YAHOO);
+		
+		int total       = StockUtil.getSymbolList().size();
+		int count       = 0;
+		int countGoogle = 0;
+		int countYahoo  = 0;
+		
+		int lastOutputCount = -1;
+		int showInterval = 100;
+
 		logger.info("MERGE");
 		for(String symbol: StockUtil.getSymbolList()) {
-			mergeFile(symbol);
+			int outputCount = count / showInterval;
+			boolean showOutput;
+			if (outputCount != lastOutputCount) {
+				showOutput = true;
+				lastOutputCount = outputCount;
+			} else {
+				showOutput = false;
+			}
+			count++;
+			
+			File file = Price.getFile(symbol);
+			
+			File priceGoogle   = priceGoogleProvider.getFile(symbol);
+			File priceYahoo    = priceYahooProvider.getFile(symbol);
+			
+			List<Price> priceList = null;
+			
+			if (priceGoogle.exists() && priceYahoo.exists()) {
+				// both
+				List<Price> priceListGoogle = Price.load(priceGoogle);
+				List<Price> priceListYahoo  = Price.load(priceYahoo);
+				
+				String dateGoogle = priceListGoogle.get(0).date;
+				String dateYahoo  = priceListYahoo.get(0).date;
+				
+				if (dateGoogle.equals(UpdateProvider.DATE_LAST) && dateYahoo.equals(UpdateProvider.DATE_LAST)) {
+					// both
+					int priceGoogleSize = priceListGoogle.size();
+					int priceYahooSize  = priceListYahoo.size();
+					
+					if (priceGoogleSize <= priceYahooSize) { // prefer yahoo over google
+						priceList = priceListYahoo;
+						countYahoo++;
+					} else {
+						priceList = priceListGoogle;
+						countGoogle++;
+					}
+				} else if (dateGoogle.equals(UpdateProvider.DATE_LAST)) {
+					// google
+					priceList = priceListGoogle;
+					countGoogle++;
+				} else if (dateYahoo.equals(UpdateProvider.DATE_LAST)) {
+					// yahoo
+					priceList = priceListYahoo;
+					countYahoo++;
+				} else {
+					// none -- could be happen for discontinued stock
+					int priceGoogleSize = priceListGoogle.size();
+					int priceYahooSize  = priceListYahoo.size();
+					
+					if (priceGoogleSize < priceYahooSize) {
+						priceList = priceListYahoo;
+						countYahoo++;
+					} else {
+						priceList = priceListGoogle;
+						countGoogle++;
+					}
+				}
+			} else if (priceGoogle.exists()) {
+				// only google
+				priceList = Price.load(priceGoogle);
+				countGoogle++;
+			} else if (priceYahoo.exists()) {
+				// only yahoo
+				priceList = Price.load(priceYahoo);
+				countYahoo++;
+			} else {
+				// none
+//				if (showOutput) logger.warn("{}  skip  {}", String.format("%4d / %4d",  count, total), symbol);
+				continue;
+			}
+			
+			if (priceList != null) {
+				if (showOutput) logger.info("{}  save  {}", String.format("%4d / %4d",  count, total), symbol);
+				Price.save(priceList, file);
+			}
 		}
 		
-		logger.info("symbol {}", String.format("%4d", StockUtil.getSymbolList().size()));
+		logger.info("total  {}", String.format("%4d", total));
 		logger.info("count  {}", String.format("%4d", count));
 		logger.info("yahoo  {}", String.format("%4d", countYahoo));
 		logger.info("google {}", String.format("%4d", countGoogle));

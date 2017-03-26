fea64446b168227d2de1633d0a2a7fdc82c0d270
yokwe <hasegawa.yasuhiro+github.com>
2017-03-26 15:04:33 +0900

    Define natural order of Activity by implementing Comparable.

--- Securities/src/yokwe/finance/securities/tax/Activity.java
+++ Securities/src/yokwe/finance/securities/tax/Activity.java
@@ -6,7 +6,7 @@ import yokwe.finance.securities.libreoffice.SpreadSheet;
 @Sheet.SheetName("Transaction")
 @Sheet.HeaderRow(0)
 @Sheet.DataRow(1)
-public class Activity extends Sheet {
+public class Activity extends Sheet implements Comparable<Activity> {
 	@ColumnName("YYYY-MM")
 	public String yyyyMM;
 	
@@ -45,8 +45,16 @@ public class Activity extends Sheet {
 	@ColumnName("Credit")
 	public double credit;
 	
+	@Override
 	public String toString() {
 		return String.format("%s %s %-8s %s %s %-8s %-60s %7.2f %8.4f %5.2f %8.2f %8.2f",
 				yyyyMM, page, transaction, date, tradeDate, symbol, name, quantity, price, commission, debit, credit);
+	}
+
+	@Override
+	public int compareTo(Activity that) {
+		String thisDate = (this.tradeDate != null && this.tradeDate.length() != 0) ? this.tradeDate : this.date;
+		String thatDate = (that.tradeDate != null && that.tradeDate.length() != 0) ? that.tradeDate : that.date;
+		return thisDate.compareTo(thatDate);
 	}
 }

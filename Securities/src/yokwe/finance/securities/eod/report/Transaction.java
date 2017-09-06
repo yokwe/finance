f0ea03ceedd61e0a141e3ba462f024a31f96934c
yokwe <hasegawa.yasuhiro+github@gmail.com>
2017-09-06 10:04:22 +0900

    Move Report.getTransactinList() to Transaction class.

--- Securities/src/yokwe/finance/securities/eod/report/Transaction.java
+++ Securities/src/yokwe/finance/securities/eod/report/Transaction.java
@@ -1,9 +1,22 @@
 package yokwe.finance.securities.eod.report;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
+
+import yokwe.finance.securities.SecuritiesException;
+import yokwe.finance.securities.eod.Market;
+import yokwe.finance.securities.libreoffice.Sheet;
+import yokwe.finance.securities.libreoffice.SpreadSheet;
+import yokwe.finance.securities.tax.Activity;
+import yokwe.finance.securities.util.DoubleUtil;
+
 public class Transaction {
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Transaction.class);
+
 	public enum Type {
 		WIRE_IN, WIRE_OUT, ACH_IN, ACH_OUT,
 		INTEREST, DIVIDEND, BUY, SELL, CHANGE,
@@ -53,8 +66,8 @@ public class Transaction {
 	
 	@Override
 	public String toString() {
-		return String.format("%-9s %10s %-10s %-10s %10.5f %8.2f %8.2f %8.2f %2d %-9s %-9s %8.2f",
-				type, date, symbol, name, quantity, debit, credit, sellCost, positionList.size(), newSymbol, newName, newQuantity);
+		return String.format("%-9s %10s %-10s %10.5f %8.2f %8.2f %8.2f %2d %-10s %8.2f",
+				type, date, symbol, quantity, debit, credit, sellCost, positionList.size(), newSymbol, newQuantity);
 	}
 	
 	public static Transaction buy(String date, String symbol, String name, double quantity, double debit, List<Position> positionList) {
@@ -83,5 +96,173 @@ public class Transaction {
 	}
 	public static Transaction change(String date, String symbol, String name, double quantity, String newSymbol, String newName, double newQuantity, List<Position> positionList) {
 		return new Transaction(Type.CHANGE, date, symbol, name, quantity, 0, 0, 0, positionList, newSymbol, newName, newQuantity);
+	}
+	
+	public static List<Transaction> getTransactionList(SpreadSheet docActivity) {
+		List<Transaction> transactionList = new ArrayList<>();
+		
+		List<String> sheetNameList = docActivity.getSheetNameList();
+		sheetNameList.sort((a, b) -> a.compareTo(b));
+		
+		for(String sheetName: sheetNameList) {
+			if (!sheetName.matches("^20[0-9][0-9]$")) {
+				logger.warn("Sheet {} skip", sheetName);
+				continue;
+			}
+			logger.info("Sheet {}", sheetName);
+			
+			List<Activity> activityList = Sheet.extractSheet(docActivity, Activity.class, sheetName);
+			// Need to sort activityList to adjust item order of activityList
+			Collections.sort(activityList);
+			
+			for(Iterator<Activity> iterator = activityList.iterator(); iterator.hasNext();) {
+				Activity activity = iterator.next();
+				
+				// Sanity check
+				{
+					if (activity.date != null && 0 < activity.date.length()) {
+						String date = activity.date;
+						if (Market.isClosed(date)) {
+							logger.error("Market is closed - date -  {}", activity);
+							throw new SecuritiesException("Market is closed");
+						}
+					}
+					if (activity.tradeDate != null && 0 < activity.tradeDate.length()) {
+						String date = activity.tradeDate;
+						if (Market.isClosed(date)) {
+							logger.error("Market is closed - tradeDate -  {}", activity);
+							throw new SecuritiesException("Market is closed");
+						}
+					}
+				}
+				switch(activity.transaction) {
+				// TODO temporary ignore old "NAME CHG"
+				case "NAME CHG": {
+					break;
+				}
+				// TODO use "NAME CHG" and "MERGER" after everything works as expected.
+				case "*NAME CHG":
+				case "*MERGER": {
+					Activity nextActivity = iterator.next();
+					if (nextActivity.date.equals(activity.date) && nextActivity.transaction.equals(activity.transaction)) {
+						String date        = activity.date;
+						String newSymbol   = activity.symbol;
+						String newName     = activity.name;
+						double newQuantity = activity.quantity;
+						
+						String symbol      = nextActivity.symbol;
+						String name        = nextActivity.name;
+						double quantity    = nextActivity.quantity;
+						
+						Stock.change(date, symbol, quantity, newSymbol, newQuantity);
+						
+						Transaction transaction = Transaction.change(date, symbol, name, quantity, newSymbol, newName, newQuantity, Stock.getPositionList());
+						logger.info("transaction {}", transaction);
+						transactionList.add(transaction);
+					} else {
+						logger.error("Unexpect transaction  {}  {}", activity.transaction, nextActivity);
+						logger.error("activity  {}", activity);
+						logger.error("next      {}", nextActivity);
+						throw new SecuritiesException("Unexpected");
+					}
+					
+					break;
+				}
+				case "BOUGHT": {
+//					logger.info("activity {} {} {} {} {}", sheetName, activity.date, activity.transaction, activity.symbol, activity.quantity);
+					String date     = activity.tradeDate;
+					String symbol   = activity.symbol;
+					String name     = activity.name;
+					double quantity = activity.quantity;
+					double total    = DoubleUtil.round((activity.price * activity.quantity) + activity.commission, 2);
+					
+					Stock.buy(date, symbol, quantity, total);
+					Transaction transaction = Transaction.buy(date, symbol, name, quantity, total, Stock.getPositionList());
+					logger.info("transaction {}", transaction);
+					transactionList.add(transaction);
+					break;
+				}
+				case "SOLD":
+				case "REDEEMED": {
+//					logger.info("activity {} {} {} {} {}", sheetName, activity.date, activity.transaction, activity.symbol, activity.quantity);
+					String date     = activity.tradeDate;
+					String symbol   = activity.symbol;
+					String name     = activity.name;
+					double quantity = activity.quantity;
+					double total    = DoubleUtil.round((activity.price * activity.quantity) - activity.commission, 2);
+
+					double sellCost = Stock.sell(date, symbol, quantity, total);
+					Transaction transaction = Transaction.sell(date, symbol, name, quantity, total, sellCost, Stock.getPositionList());
+					logger.info("transaction {}", transaction);
+					transactionList.add(transaction);
+					break;
+				}
+				case "INTEREST": {
+					String date     = activity.date;
+					double credit   = activity.credit;
+
+					Transaction transaction = Transaction.interest(date, credit);
+					logger.info("transaction {}", transaction);
+					transactionList.add(transaction);
+					break;
+				}
+				case "DIVIDEND":
+				case "ADR":
+				case "MLP":
+				case "NRA":
+				case "CAP GAIN": {
+					String date     = activity.date;
+					String symbol   = activity.symbol;
+					String name     = activity.name;
+					double quantity = activity.quantity;
+					double debit    = activity.debit;
+					double credit   = activity.credit;
+
+					Transaction transaction = Transaction.dividend(date, symbol, name, quantity, debit, credit);
+					logger.info("transaction {}", transaction);
+					transactionList.add(transaction);
+					break;
+				}
+				case "ACH": {
+					String date     = activity.date;
+					double debit    = activity.debit;
+					double credit   = activity.credit;
+					
+					if (debit != 0) {
+						Transaction transaction = Transaction.achOut(date, debit);
+						logger.info("transaction {}", transaction);
+						transactionList.add(transaction);
+					}
+					if (credit != 0) {
+						Transaction transaction = Transaction.achIn(date, credit);
+						logger.info("transaction {}", transaction);
+						transactionList.add(transaction);
+					}
+					break;
+				}
+				case "WIRE": {
+					String date     = activity.date;
+					double debit    = activity.debit;
+					double credit   = activity.credit;
+					
+					if (debit != 0) {
+						Transaction transaction = Transaction.wireOut(date, debit);
+						logger.info("transaction {}", transaction);
+						transactionList.add(transaction);
+					}
+					if (credit != 0) {
+						Transaction transaction = Transaction.wireIn(date, credit);
+						logger.info("transaction {}", transaction);
+						transactionList.add(transaction);
+					}
+					break;
+				}
+				default:
+					logger.error("Unknown transaction {}", activity.transaction);
+					throw new SecuritiesException("Unknown transaction");
+				}
+			}
+		}
+		return transactionList;
 	}
 }
--- Securities/src/yokwe/finance/securities/eod/tax/Report.java
+++ Securities/src/yokwe/finance/securities/eod/tax/Report.java
@@ -3,8 +3,6 @@ package yokwe.finance.securities.eod.tax;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
@@ -14,17 +12,12 @@ import java.util.stream.Collectors;
 
 import org.slf4j.LoggerFactory;
 
-import yokwe.finance.securities.SecuritiesException;
 import yokwe.finance.securities.eod.ForexUtil;
-import yokwe.finance.securities.eod.Market;
-import yokwe.finance.securities.eod.report.Stock;
 import yokwe.finance.securities.eod.report.Transaction;
 import yokwe.finance.securities.libreoffice.Sheet;
 import yokwe.finance.securities.libreoffice.SpreadSheet;
-import yokwe.finance.securities.tax.Activity;
 import yokwe.finance.securities.tax.Dividend;
 import yokwe.finance.securities.tax.Interest;
-import yokwe.finance.securities.util.DoubleUtil;
 
 public class Report {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Report.class);
@@ -36,174 +29,6 @@ public class Report {
 	public static final String URL_TEMPLATE      = "file:///home/hasegawa/Dropbox/Trade/TAX_REPORT_TEMPLATE.ods";
 	public static final String URL_REPORT        = String.format("file:///home/hasegawa/Dropbox/Trade/Report/EOD_TAX_%s.ods", TIMESTAMP);
 
-	private static List<Transaction> getTransactionList(SpreadSheet docActivity) {
-		List<Transaction> transactionList = new ArrayList<>();
-		
-		List<String> sheetNameList = docActivity.getSheetNameList();
-		sheetNameList.sort((a, b) -> a.compareTo(b));
-		
-		for(String sheetName: sheetNameList) {
-			if (!sheetName.matches("^20[0-9][0-9]$")) {
-				logger.warn("Sheet {} skip", sheetName);
-				continue;
-			}
-			logger.info("Sheet {}", sheetName);
-			
-			List<Activity> activityList = Sheet.extractSheet(docActivity, Activity.class, sheetName);
-			// Need to sort activityList to adjust item order of activityList
-			Collections.sort(activityList);
-			
-			for(Iterator<Activity> iterator = activityList.iterator(); iterator.hasNext();) {
-				Activity activity = iterator.next();
-				
-				// Sanity check
-				{
-					if (activity.date != null && 0 < activity.date.length()) {
-						String date = activity.date;
-						if (Market.isClosed(date)) {
-							logger.error("Market is closed - date -  {}", activity);
-							throw new SecuritiesException("Market is closed");
-						}
-					}
-					if (activity.tradeDate != null && 0 < activity.tradeDate.length()) {
-						String date = activity.tradeDate;
-						if (Market.isClosed(date)) {
-							logger.error("Market is closed - tradeDate -  {}", activity);
-							throw new SecuritiesException("Market is closed");
-						}
-					}
-				}
-				switch(activity.transaction) {
-				// TODO temporary ignore old "NAME CHG"
-				case "NAME CHG": {
-					break;
-				}
-				// TODO use "NAME CHG" and "MERGER" after everything works as expected.
-				case "*NAME CHG":
-				case "*MERGER": {
-					Activity nextActivity = iterator.next();
-					if (nextActivity.date.equals(activity.date) && nextActivity.transaction.equals(activity.transaction)) {
-						String date        = activity.date;
-						String newSymbol   = activity.symbol;
-						String newName     = activity.name;
-						double newQuantity = activity.quantity;
-						
-						String symbol      = nextActivity.symbol;
-						String name        = nextActivity.name;
-						double quantity    = nextActivity.quantity;
-						
-						Stock.change(date, symbol, quantity, newSymbol, newQuantity);
-						
-						Transaction transaction = Transaction.change(date, symbol, name, quantity, newSymbol, newName, newQuantity, Stock.getPositionList());
-						logger.info("transaction {}", transaction);
-						transactionList.add(transaction);
-					} else {
-						logger.error("Unexpect transaction  {}  {}", activity.transaction, nextActivity);
-						logger.error("activity  {}", activity);
-						logger.error("next      {}", nextActivity);
-						throw new SecuritiesException("Unexpected");
-					}
-					
-					break;
-				}
-				case "BOUGHT": {
-//					logger.info("activity {} {} {} {} {}", sheetName, activity.date, activity.transaction, activity.symbol, activity.quantity);
-					String date     = activity.tradeDate;
-					String symbol   = activity.symbol;
-					String name     = activity.name;
-					double quantity = activity.quantity;
-					double total    = DoubleUtil.round((activity.price * activity.quantity) + activity.commission, 2);
-					
-					Stock.buy(date, symbol, quantity, total);
-					Transaction transaction = Transaction.buy(date, symbol, name, quantity, total, Stock.getPositionList());
-					logger.info("transaction {}", transaction);
-					transactionList.add(transaction);
-					break;
-				}
-				case "SOLD":
-				case "REDEEMED": {
-//					logger.info("activity {} {} {} {} {}", sheetName, activity.date, activity.transaction, activity.symbol, activity.quantity);
-					String date     = activity.tradeDate;
-					String symbol   = activity.symbol;
-					String name     = activity.name;
-					double quantity = activity.quantity;
-					double total    = DoubleUtil.round((activity.price * activity.quantity) - activity.commission, 2);
-
-					double sellCost = Stock.sell(date, symbol, quantity, total);
-					Transaction transaction = Transaction.sell(date, symbol, name, quantity, total, sellCost, Stock.getPositionList());
-					logger.info("transaction {}", transaction);
-					transactionList.add(transaction);
-					break;
-				}
-				case "INTEREST": {
-					String date     = activity.date;
-					double credit   = activity.credit;
-
-					Transaction transaction = Transaction.interest(date, credit);
-					logger.info("transaction {}", transaction);
-					transactionList.add(transaction);
-					break;
-				}
-				case "DIVIDEND":
-				case "ADR":
-				case "MLP":
-				case "NRA":
-				case "CAP GAIN": {
-					String date     = activity.date;
-					String symbol   = activity.symbol;
-					String name     = activity.name;
-					double quantity = activity.quantity;
-					double debit    = activity.debit;
-					double credit   = activity.credit;
-
-					Transaction transaction = Transaction.dividend(date, symbol, name, quantity, debit, credit);
-					logger.info("transaction {}", transaction);
-					transactionList.add(transaction);
-					break;
-				}
-				case "ACH": {
-					String date     = activity.date;
-					double debit    = activity.debit;
-					double credit   = activity.credit;
-					
-					if (debit != 0) {
-						Transaction transaction = Transaction.achOut(date, debit);
-						logger.info("transaction {}", transaction);
-						transactionList.add(transaction);
-					}
-					if (credit != 0) {
-						Transaction transaction = Transaction.achIn(date, credit);
-						logger.info("transaction {}", transaction);
-						transactionList.add(transaction);
-					}
-					break;
-				}
-				case "WIRE": {
-					String date     = activity.date;
-					double debit    = activity.debit;
-					double credit   = activity.credit;
-					
-					if (debit != 0) {
-						Transaction transaction = Transaction.wireOut(date, debit);
-						logger.info("transaction {}", transaction);
-						transactionList.add(transaction);
-					}
-					if (credit != 0) {
-						Transaction transaction = Transaction.wireIn(date, credit);
-						logger.info("transaction {}", transaction);
-						transactionList.add(transaction);
-					}
-					break;
-				}
-				default:
-					logger.error("Unknown transaction {}", activity.transaction);
-					throw new SecuritiesException("Unknown transaction");
-				}
-			}
-		}
-		return transactionList;
-	}
-	
 	static Map<String, Interest> getInterestMap(List<Transaction> transactionList) {
 		Map<String, Interest> ret = new TreeMap<>();
 		
@@ -246,7 +71,7 @@ public class Report {
 			SpreadSheet docLoad = new SpreadSheet(URL_TEMPLATE, true);
 			SpreadSheet docSave = new SpreadSheet();
 
-			List<Transaction> transactionList = getTransactionList(docActivity);
+			List<Transaction> transactionList = Transaction.getTransactionList(docActivity);
 			
 			// key is date
 			Map<String, Dividend> dividendMap = getDividendMap(transactionList);

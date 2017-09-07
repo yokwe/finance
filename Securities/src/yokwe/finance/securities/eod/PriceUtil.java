8906867e7c9421e356fd208ba7af5d1fa139b9de
yokwe <hasegawa.yasuhiro+github@gmail.com>
2017-09-07 13:57:10 +0900

    Add PriceUtil.getLastPrice()

--- Securities/src/yokwe/finance/securities/eod/PriceUtil.java
+++ Securities/src/yokwe/finance/securities/eod/PriceUtil.java
@@ -2,6 +2,7 @@ package yokwe.finance.securities.eod;
 
 import java.io.File;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.TreeMap;
 
 import org.slf4j.LoggerFactory;
@@ -11,7 +12,7 @@ import yokwe.finance.securities.SecuritiesException;
 public class PriceUtil {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(PriceUtil.class);
 
-	private static Map<String, Map<String, Price>> priceMap = new TreeMap<>();
+	private static Map<String, NavigableMap<String, Price>> priceMap = new TreeMap<>();
 	
 	public static boolean contains(String symbol, String date) {
 		symbol = symbol.replace(".PR.", "-");
@@ -19,9 +20,13 @@ public class PriceUtil {
 		if (!priceMap.containsKey(symbol)) {
 			File file = Price.getFile(symbol);
 			if (file.canRead()) {
-				Map<String, Price> map = new TreeMap<>();
+				NavigableMap<String, Price> map = new TreeMap<>();
 				for(Price price: Price.load(file)) {
 					map.put(price.date, price);
+				}
+				if(map.isEmpty()) {
+					logger.error("map is empty {}", symbol);
+					throw new SecuritiesException("Unexpected");
 				}
 				priceMap.put(symbol, map);
 			} else {
@@ -37,22 +42,30 @@ public class PriceUtil {
 			return false;
 		}
 	}
+	
+	private static void fillMap(String symbol) {
+		if (priceMap.containsKey(symbol)) return;
+		
+		File file = Price.getFile(symbol);
+		if (file.canRead()) {
+			NavigableMap<String, Price> map = new TreeMap<>();
+			for(Price price: Price.load(file)) {
+				map.put(price.date, price);
+			}
+			if(map.isEmpty()) {
+				logger.error("map is empty {}", symbol);
+				throw new SecuritiesException("Unexpected");
+			}
+			priceMap.put(symbol, map);
+		} else {
+			logger.warn("no price file  {}", file.getPath());
+			throw new SecuritiesException("Unexpected");
+		}
+	}
 	public static Price getPrice(String symbol, String date) {
 		symbol = symbol.replace(".PR.", "-");
 
-		if (!priceMap.containsKey(symbol)) {
-			File file = Price.getFile(symbol);
-			if (file.canRead()) {
-				Map<String, Price> map = new TreeMap<>();
-				for(Price price: Price.load(file)) {
-					map.put(price.date, price);
-				}
-				priceMap.put(symbol, map);
-			} else {
-				logger.error("Unexpected symbol {}", symbol);
-				throw new SecuritiesException("Unexpected symbol");
-			}
-		}
+		fillMap(symbol);
 		Map<String, Price> map = priceMap.get(symbol);
 		if (map.containsKey(date)) {
 			return map.get(date);
@@ -63,5 +76,13 @@ public class PriceUtil {
 	}
 	public static double getClose(String symbol, String date) {
 		return getPrice(symbol, date).close;
+	}
+	
+	public static Price getLastPrice(String symbol) {
+		symbol = symbol.replace(".PR.", "-");
+
+		fillMap(symbol);
+		NavigableMap<String, Price> map = priceMap.get(symbol);
+		return map.lastEntry().getValue();
 	}
 }

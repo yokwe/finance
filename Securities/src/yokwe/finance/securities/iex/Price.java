03d712bc81a05f305c06838c863c75357a9c7175
hasegawa <hasegawa@ubuntu-dev>
2018-07-29 11:50:46 +0900

    Add Price.

---
+++ Securities/src/yokwe/finance/securities/iex/Price.java
@@ -0,0 +1,43 @@
+package yokwe.finance.securities.iex;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import yokwe.finance.securities.util.HttpUtil;
+
+public class Price extends IEXBase {
+	public double current; // A single number, being the IEX real time price, the 15 minute delayed market price, or the previous close price, is returned.
+	
+	Price(String current) {
+		this.current = Double.valueOf(current);
+	}
+	
+	public static Price getStock(String symbol) {
+		String url = String.format("%s/stock/%s/price", END_POINT, symbol);
+		String jsonString = HttpUtil.downloadAsString(url);
+		
+		return new Price(jsonString);
+	}
+
+	static void test(Logger logger) {
+		String jsonString = "145.15";
+		logger.info("json {}", jsonString);
+		
+		Price price = new Price(jsonString);
+		logger.info("price {}", price.toString());
+	}
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(Price.class);
+		logger.info("START");
+		
+		test(logger);
+		
+		{
+			Price price = Price.getStock("ibm");
+			logger.info("price {}", price.toString());
+		}
+
+		logger.info("STOP");
+	}
+
+}

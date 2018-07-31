cc80ebd026ee7410bc0851042c6fc7198eec4dfd
hasegawa <hasegawa@ubuntu-dev>
2018-07-31 21:51:53 +0900

    Move delisted files to PATH_DELISTED_DIR.

--- Securities/src/yokwe/finance/securities/iex/UpdateChart.java
+++ Securities/src/yokwe/finance/securities/iex/UpdateChart.java
@@ -1,20 +1,28 @@
 package yokwe.finance.securities.iex;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.slf4j.LoggerFactory;
 
+import yokwe.finance.securities.SecuritiesException;
 import yokwe.finance.securities.iex.IEXBase.Range;
 import yokwe.finance.securities.util.CSVUtil;
 
 public class UpdateChart {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(UpdateChart.class);
 	
-	public static final String PATH_DIR = "tmp/iex/chart";
+	public static final String PATH_DIR          = "tmp/iex/chart";
+	public static final String PATH_DELISTED_DIR = "tmp/iex/chart-delisted";
 	
 	private static String getFilePath(String symbol) {
 		return String.format("%s/%s.csv", PATH_DIR, symbol);
@@ -28,6 +36,39 @@ public class UpdateChart {
 		List<String> symbolList = UpdateSymbols.getSymbolList();
 		int symbolListSize = symbolList.size();
 		logger.info("symbolList {}", symbolList.size());
+		
+		// Remove unknown file
+		{
+			File dir = new File(PATH_DIR);
+			Set<String> symbolSet = new TreeSet<>(symbolList);
+			
+			File dirDelisted = new File(PATH_DELISTED_DIR);
+			if (!dirDelisted.exists()) {
+				dirDelisted.mkdirs();
+			}
+			
+			String destFileSuffix = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
+			List<File> fileList = Arrays.asList(dir.listFiles((d, name) -> (name.endsWith(".csv"))));
+			fileList.sort((a, b) -> a.getName().compareTo(b.getName()));
+			try {
+				for(File file: fileList) {
+					String name = file.getName();
+					String symbol = name.replace(".csv", "");
+					if (symbolSet.contains(symbol)) continue;
+					
+						File destFile = new File(PATH_DELISTED_DIR, String.format("%s-%s", name, destFileSuffix));
+						logger.info("move unknown file {} to {}", file.getPath(), destFile.getPath());
+											
+						// Copy file to new location
+						Files.copy(file.toPath(), destFile.toPath());
+						// Delete file after successful copy
+						file.delete();
+				}
+			} catch (IOException e) {
+				logger.error("IOException {}", e.toString());
+				throw new SecuritiesException("IOException");
+			}
+		}
 		
 		int countGetStock = 0;
 		int countData = 0;

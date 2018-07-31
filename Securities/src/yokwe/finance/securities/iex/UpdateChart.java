9f47f952533065f7d4cd190b6e65fc6fb211e2c5
hasegawa <hasegawa@ubuntu-dev>
2018-07-31 20:10:30 +0900

    Add UpdateChart class.

---
+++ Securities/src/yokwe/finance/securities/iex/UpdateChart.java
@@ -0,0 +1,60 @@
+package yokwe.finance.securities.iex;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.LoggerFactory;
+
+import yokwe.finance.securities.iex.IEXBase.Range;
+import yokwe.finance.securities.util.CSVUtil;
+
+public class UpdateChart {
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(UpdateChart.class);
+	
+	public static final String PATH_DIR = "tmp/iex/chart";
+	
+	private static String getFilePath(String symbol) {
+		return String.format("%s/%s.csv", PATH_DIR, symbol);
+	}
+	
+	private static final int DELTA = 10;
+	
+	public static void main (String[] args) {
+		logger.info("START");
+		
+		List<String> symbolList = UpdateSymbols.getSymbolList();
+		int symbolListSize = symbolList.size();
+		logger.info("symbolList {}", symbolList.size());
+		
+		int countGetStock = 0;
+		int countData = 0;
+		for(int i = 0; i < symbolListSize; i += DELTA) {
+			int fromIndex = i;
+			int toIndex = Math.min(fromIndex + DELTA, symbolListSize);
+			
+			List<String> getList = new ArrayList<>();
+			for(String symbol: symbolList.subList(fromIndex, toIndex)) {
+				File file = new File(getFilePath(symbol));
+				if (file.exists()) continue;
+				getList.add(symbol);
+			}
+			if (getList.isEmpty()) continue;
+			logger.info("  {} ({}){}", fromIndex, getList.size(), getList.toString());
+			countGetStock += getList.size();
+			
+			Map<String, Chart[]> chartMap = Chart.getStock(Range.Y1, getList.toArray(new String[0]));
+			for(Map.Entry<String, Chart[]>entry: chartMap.entrySet()) {
+				List<Chart> dataList = Arrays.asList(entry.getValue());
+				if (dataList.size() == 0) continue;
+				CSVUtil.saveWithHeader(dataList, getFilePath(entry.getKey()));
+				countData++;
+			}
+		}
+
+		logger.info("stats {} / {}", countData, countGetStock);
+		logger.info("STOP");
+	}
+}

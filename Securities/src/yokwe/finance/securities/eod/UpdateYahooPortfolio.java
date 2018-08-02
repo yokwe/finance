697003df93192ff72d2d2495241089be9be84231
hasegawa <hasegawa@ubuntu-dev>
2018-08-02 21:24:34 +0900

    Add YahooPortfolio and UpdateYahooPortfolio.

---
+++ Securities/src/yokwe/finance/securities/eod/UpdateYahooPortfolio.java
@@ -0,0 +1,44 @@
+package yokwe.finance.securities.eod;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.LoggerFactory;
+
+import yokwe.finance.securities.util.CSVUtil;
+
+public class UpdateYahooPortfolio {
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(UpdateYahooPortfolio.class);
+	
+	public static final String PATH_YAHOO_PORTFOLIO = "tmp/eod/yahoo-portfolio.csv";
+	public static final String PATH_STOCK_HISTORY   = "tmp/eod/stock-history.csv";
+
+	public static void main(String[] args) {
+		logger.info("START");
+		
+		List<YahooPortfolio> yahooPortfolioList = new ArrayList<>();
+		
+		Map<String, List<StockHistory>> stockHistoryMap = UpdateStockHistory.getStockHistoryMap();
+		logger.info("stockHistoryMap {}", stockHistoryMap.size());
+		
+		for(Map.Entry<String, List<StockHistory>> entry: stockHistoryMap.entrySet()) {
+			List<StockHistory> stockHistoryList = entry.getValue();
+			
+			StockHistory lastStockHistory = stockHistoryList.get(stockHistoryList.size() - 1);
+			if (lastStockHistory.totalQuantity == 0) continue;
+			
+			// Change symbol style from iex to yahoo
+			String symbol        = entry.getKey().replaceAll("-", "-P");
+			double quantity      = lastStockHistory.totalQuantity;
+			double purchasePrice = lastStockHistory.totalCost / lastStockHistory.totalQuantity;
+			
+			yahooPortfolioList.add(new YahooPortfolio(symbol, purchasePrice, quantity));
+		}
+		
+		CSVUtil.saveWithHeader(yahooPortfolioList, PATH_YAHOO_PORTFOLIO);
+		logger.info("yahooPortfolioList {}", yahooPortfolioList.size());
+		
+		logger.info("STOP");
+	}
+}

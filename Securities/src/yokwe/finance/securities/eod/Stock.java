007e8c52c06372e5f70f53e93bb9b15facb5bc84
hasegawa <hasegawa@ubuntu-dev>
2018-08-03 14:11:19 +0900

    Implements Comparable<Stock> for natural order sort.

--- Securities/src/yokwe/finance/securities/eod/Stock.java
+++ Securities/src/yokwe/finance/securities/eod/Stock.java
@@ -1,12 +1,17 @@
 package yokwe.finance.securities.eod;
 
-public class Stock {
+public class Stock implements Comparable<Stock> {
 	public String symbol;
 	public String exchange;
 	public String issueType;
 	public String sector;
 	public String industry;
 	public String name;
+
+	@Override
+	public int compareTo(Stock that) {
+		return this.symbol.compareTo(that.symbol);
+	}
 		
 	@Override
 	public String toString() {

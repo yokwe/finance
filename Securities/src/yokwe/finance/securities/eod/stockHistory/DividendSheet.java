79be427ca30f57ddda7ac428c71f72b6a03327b7
hasegawa <hasegawa@ubuntu-dev>
2018-08-06 20:27:42 +0900

    Add ColumnName and make public for fields.

--- Securities/src/yokwe/finance/securities/eod/stockHistory/DividendSheet.java
+++ Securities/src/yokwe/finance/securities/eod/stockHistory/DividendSheet.java
@@ -4,85 +4,113 @@ import yokwe.finance.securities.libreoffice.Sheet;
 import yokwe.finance.securities.libreoffice.SpreadSheet;
 
 @Sheet.SheetName("Dividend")
-@Sheet.HeaderRow(3)
-@Sheet.DataRow(4)
+@Sheet.HeaderRow(2)
+@Sheet.DataRow(2)
 public class DividendSheet extends Sheet {
 	@ColumnName("symbol")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String symbol;
+	public String symbol;
 	
 	@ColumnName("quantity")
 	@NumberFormat(SpreadSheet.FORMAT_INTEGER)
-	Double quantity;
+	public Double quantity;
 	
 	@ColumnName("cost")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double cost;
+	public Double cost;
+	
+	@ColumnName("div")
+	@NumberFormat(SpreadSheet.FORMAT_USD)
+	public Double div;
 	
 	@ColumnName("interest")
 	@NumberFormat(SpreadSheet.FORMAT_PERCENT)
-	Double interest;
+	public Double interest;
 	
 	
+	@ColumnName("pay1")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay1;
+	public String pay1;
+	@ColumnName("div1")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div1;
+	public Double div1;
 	
+	@ColumnName("pay2")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay2;
+	public String pay2;
+	@ColumnName("div2")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div2;
+	public Double div2;
 	
+	@ColumnName("pay3")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay3;
+	public String pay3;
+	@ColumnName("div3")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div3;
+	public Double div3;
 	
+	@ColumnName("pay4")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay4;
+	public String pay4;
+	@ColumnName("div4")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div4;
+	public Double div4;
 	
+	@ColumnName("pay5")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay5;
+	public String pay5;
+	@ColumnName("div5")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div5;
+	public Double div5;
 	
+	@ColumnName("pay6")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay6;
+	public String pay6;
+	@ColumnName("div6")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div6;
+	public Double div6;
 	
+	@ColumnName("pay7")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay7;
+	public String pay7;
+	@ColumnName("div7")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div7;
+	public Double div7;
 	
+	@ColumnName("pay8")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay8;
+	public String pay8;
+	@ColumnName("div8")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div8;
+	public Double div8;
 	
+	@ColumnName("pay9")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay9;
+	public String pay9;
+	@ColumnName("div9")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div9;
+	public Double div9;
 	
+	@ColumnName("pay10")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay10;
+	public String pay10;
+	@ColumnName("div10")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div10;
+	public Double div10;
 	
+	@ColumnName("pay11")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay11;
+	public String pay11;
+	@ColumnName("div11")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div11;
+	public Double div11;
 	
+	@ColumnName("pay12")
 	@NumberFormat(SpreadSheet.FORMAT_STRING)
-	String pay12;
+	public String pay12;
+	@ColumnName("div12")
 	@NumberFormat(SpreadSheet.FORMAT_USD)
-	Double div12;
+	public Double div12;
 	
 	DividendSheet(String symbol, Double quantity, Double cost, Double interest,
 		String pay1, Double div1, String pay2, Double div2, String pay3, Double div3, 

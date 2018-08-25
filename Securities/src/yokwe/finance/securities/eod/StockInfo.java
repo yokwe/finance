a0a0bc9728d4024ed3442923df52acf042152b81
hasegawa <hasegawa@ubuntu-dev>
2018-08-25 17:08:37 +0900

    Remove static field from StockInfo class.

--- Securities/src/yokwe/finance/securities/eod/StockInfo.java
+++ Securities/src/yokwe/finance/securities/eod/StockInfo.java
@@ -11,38 +11,41 @@ import org.slf4j.LoggerFactory;
 public class StockInfo {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(StockInfo.class);
 	
-	private static Map<String, StockInfo> stockInfoMap;
-	static {
-		{
-			Map<String, List<StockHistory>> stockHistoryMap = UpdateStockHistory.getStockHistoryMap();
+	public static class Entry {
+		public final String             symbol;
+		public final boolean            active;
+		public final List<StockHistory> stockHistoryList;
+		public final StockHistory       lastStockHistory;
+		public final List<StockHistory> lastStockHistoryList;
+		
+		private Entry (List<StockHistory> stockHistory) {
+			StockHistory lastStockHistory = stockHistory.get(stockHistory.size() - 1);
 
-			Map<String, StockInfo> map = new TreeMap<>();
-			for(List<StockHistory> list: stockHistoryMap.values()) {
-				StockInfo activeStockHistory = new StockInfo(list);
-				map.put(activeStockHistory.symbol, activeStockHistory);
-			}
-			stockInfoMap = Collections.unmodifiableMap(map);
+			this.symbol               = lastStockHistory.group;
+			this.stockHistoryList     = Collections.unmodifiableList(stockHistory);
+			this.lastStockHistory     = lastStockHistory;
+			this.lastStockHistoryList = Collections.unmodifiableList(stockHistoryList.stream().filter(o -> o.session == lastStockHistory.session).collect(Collectors.toList()));
+			this.active               = lastStockHistory.totalQuantity != 0;
 		}
 	}
 	
-	public static Map<String, StockInfo> getStockInfoMap() {
-		return stockInfoMap;
+	private final Map<String, Entry> entryMap;
+	public StockInfo(String pathBase) {
+		Map<String, Entry> map = new TreeMap<>();
+		
+		{
+			Map<String, List<StockHistory>> stockHistoryMap = UpdateStockHistory.getStockHistoryMap(pathBase);
+			for(List<StockHistory> list: stockHistoryMap.values()) {
+				Entry entry = new Entry(list);
+				map.put(entry.symbol, entry);
+			}
+		}
+		
+		entryMap = Collections.unmodifiableMap(map);
 	}
 	
-	public final String             symbol;
-	public final boolean            active;
-	public final List<StockHistory> stockHistoryList;
-	public final StockHistory       lastStockHistory;
-	public final List<StockHistory> lastStockHistoryList;
-	
-	private StockInfo(List<StockHistory> stockHistory) {
-		StockHistory lastStockHistory = stockHistory.get(stockHistory.size() - 1);
-
-		this.symbol               = lastStockHistory.group;
-		this.stockHistoryList     = Collections.unmodifiableList(stockHistory);
-		this.lastStockHistory     = lastStockHistory;
-		this.lastStockHistoryList = Collections.unmodifiableList(stockHistoryList.stream().filter(o -> o.session == lastStockHistory.session).collect(Collectors.toList()));
-		this.active               = lastStockHistory.totalQuantity != 0;
+	public Map<String, Entry> getStockInfoMap() {
+		return entryMap;
 	}
 	
 	public static void main(String[] args) {
@@ -50,19 +53,19 @@ public class StockInfo {
 		
 		{
 			int count_active = 0;
-			Map<String, StockInfo> stockInfoMap = getStockInfoMap();
-			for(Map.Entry<String, StockInfo> entry: stockInfoMap.entrySet()) {
-				StockInfo stockInfo = entry.getValue();
-				
-				logger.info("{} {}", String.format("%-8s", stockInfo.symbol), stockInfo.active ? "ACTIVE" : "");
-				if (stockInfo.active) {
+			StockInfo stockInfo = new StockInfo(".");
+			
+			Map<String, Entry> entryMap = stockInfo.entryMap;
+			for(Entry entry: entryMap.values()) {				
+				logger.info("{} {}", String.format("%-8s", entry.symbol), entry.active ? "ACTIVE" : "");
+				if (entry.active) {
 					count_active++;
-					for(StockHistory stockHistory: stockInfo.lastStockHistoryList) {
+					for(StockHistory stockHistory: entry.lastStockHistoryList) {
 						logger.info("  {}", stockHistory);
 					}
 				}
 			}
-			logger.info("stockHistoryInfoMap {} / {}", count_active, stockInfoMap.size());
+			logger.info("stockHistoryInfoMap {} / {}", count_active, entryMap.size());
 		}
 		
 		logger.info("STOP");

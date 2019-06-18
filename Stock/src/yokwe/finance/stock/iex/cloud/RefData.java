8d10d46cd4661ea52393375118c41c85b0804c0d
hasegawa <hasegawa@ubuntudev.local>
2019-06-18 10:56:31 +0900

    Add PATH and RefData.Symbols class.

--- Stock/src/yokwe/finance/stock/iex/cloud/RefData.java
+++ Stock/src/yokwe/finance/stock/iex/cloud/RefData.java
@@ -6,6 +6,7 @@ public class RefData {
 	public static class IEX {
 		public static class Symbols extends Base implements Comparable<Symbols> {
 			public static final String METHOD = "/ref-data/iex/symbols";
+			public static final String PATH   = "/iex-symbols.csv";
 
 			// {"symbol":"A","date":"2019-06-17","isEnabled":true}
 			public String  symbol;
@@ -28,4 +29,43 @@ public class RefData {
 			}
 		}
 	}
+	
+	
+	public static class Symbols extends Base implements Comparable<Symbols> {
+		public static final String METHOD = "/ref-data/symbols";
+		public static final String PATH   = "/symbols.csv";
+
+		// symbol,exchange,name,date,type,iexId,region,currency,isEnabled
+		public String  symbol;
+		public String  exchange;
+		public String  name;
+		public String  date;
+		public String  type;
+		public String  iexId;
+		public String  region;
+		public String  currency;
+		public boolean isEnabled;
+		
+		public Symbols() {
+			symbol    = null;
+			exchange  = null;
+			name      = null;
+			date      = null;
+			type      = null;
+			iexId     = null;
+			region    = null;
+			currency  = null;
+			isEnabled = false;
+		}
+		
+		public Symbols(JsonObject jsonObject) {
+			super(jsonObject);
+		}
+
+		@Override
+		public int compareTo(Symbols that) {
+			return this.symbol.compareTo(that.symbol);
+		}
+	}
+
 }
